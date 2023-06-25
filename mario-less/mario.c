3ce0c70ca5381d4740bcb028aac13a73bddd0358
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-25 22:54:37 +0000

    Sun, Jun 25, 2023, 6:54 PM -04:00

---
+++ mario-less/mario.c
@@ -0,0 +1,17 @@
+// Prompts user for an int size and creates a half-pyramid in the style of Mario
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string height
+    do
+    {
+
+    }
+    while
+    {
+        height < 0 || 
+    }
+}
