87af388e2f163f47f95f1e7e9a556c713378a8ba
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 21:07:10 +0000

    Mon, Jun 26, 2023, 5:07 PM -04:00

--- week2/lecture/scores.c
+++ week2/lecture/scores.c
@@ -1,15 +1,22 @@
+// Prints the average of 3 user input test scores
+
 #include <stdio.h>
 #include <cs50.h>
 
+// Returns the average of an array of 3 elements
+float average(int array[]);
+
 int main(void)
 {
-    int scores[3];
+    printf("Average: %f\n", average(int scores[]);
+}
 
+// Returns the average of an array of 3 elements
+float average(int array[])
+{
+    int scores[3];
     for (int i = 0; i < 3; i++)
     {
         scores[i] = get_int("Score %i: ", i + 1);
     }
-
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
-
 }
