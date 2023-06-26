d71c0fd32d4cbdfe864b2ebc7e006508ea1bf121
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 20:58:52 +0000

    Mon, Jun 26, 2023, 4:58 PM -04:00

--- week2/lecture/scores.c
+++ week2/lecture/scores.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int scores[3];
+    scores[0] = get_int("Score 1: ");
+    scores[1] = get_int("Score 2: ");
+    scores[2] = get_int("Score 3: ");
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3);
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 
 }
