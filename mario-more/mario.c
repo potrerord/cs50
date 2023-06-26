bc70488e1b55b827599f9dd34e170691e2144d14
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 14:25:12 +0000

    Mon, Jun 26, 2023, 10:25 AM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,52 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+// Continually prompts user for int height until positive
+int get_height(void);
+
+// Prints pyramid with height input
+void print_pyramid(int height);
+
+int main(void)
 {
-    
+    // Get height of pyramid
+    int h = get_height();
+
+    // Print pyramid
+    print_pyramid(h);
+}
+
+// Continually prompts user for int height until positive
+int get_height(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1);
+    return h;
+}
+
+// Prints half-pyramid with height input
+void print_hpyramid(int height)
+{
+    // Print input number of rows
+    for (int i = 0; i < height; i++)
+    {
+        // In each row, print decreasing spaces starting with height - 1
+        for (int j = 0; j < height - 1 - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Then print increasing # starting with 1
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+
+        // Print new line after each row
+        printf("\n");
+    }
 }
