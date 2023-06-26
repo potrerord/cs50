9eb9f1c5bbdd3f508da068e2b6c6ecbe8b932363
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 00:44:09 +0000

    Sun, Jun 25, 2023, 8:44 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -5,5 +5,8 @@
 
 int main(void)
 {
-    
+    // Subtract 25 cents until < 25 cents remaining, add coins
+    // Subtract 10 cents until < 10 cents remaining
+    // Subtract 5 cents until < 5 cents remaining
+    // Subtract 1 cent until 0 cents remaining
 }
