abdb28ad3722674e8264209fdfcc8d66922cdcaf
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 22:34:53 +0000

    Mon, Jun 26, 2023, 6:34 PM -04:00

--- week2/lecture/uppercase.c
+++ week2/lecture/uppercase.c
@@ -4,14 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 
+// bool check_upper(input);
+
 int main(void)
 {
     // Check if uppercase or lowercase
     // char input = get_char("Input a character to convert to uppercase: ");
     // if (check_upper(input) = true)
-    {
-
-    }
+    // {
+    //     print("%c\n", input);
+    // }
+    // else
+    // {
+    //     convert to uppercase
+    // }
 
     // Convert to uppercase only
 
