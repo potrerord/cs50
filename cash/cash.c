dd63c5f1839b85366a6687c87faabea3c12bf077
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 00:49:00 +0000

    Sun, Jun 25, 2023, 8:49 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,12 +1,26 @@
-// Prints the minimum coins needed to make a given amount of change (in cents)
+// Prints the minimum US coins (up to quarters) needed to make a given amount of change owed in cents
 
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
-    // Subtract 25 cents until < 25 cents remaining, add coins
-    // Subtract 10 cents until < 10 cents remaining
-    // Subtract 5 cents until < 5 cents remaining
-    // Subtract 1 cent until 0 cents remaining
+    // Get change owed in cents
+    int change = get_int("Change owed: ");
+
+    // Count number of coins with n
+    int n = 0
+
+    // Subtract 25 cents until < 25 cents remaining, increment counter accordingly
+    while ()
+
+    // Subtract 10 cents until < 10 cents remaining, increment counter accordingly
+
+
+    // Subtract 5 cents until < 5 cents remaining, increment counter accordingly
+
+
+    // Subtract 1 cent until 0 cents remaining, increment counter accordingly
+
+
 }
