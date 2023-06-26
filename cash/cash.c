11db96e759638a08e0d0f00a3648641ee22ad735
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 00:53:05 +0000

    Sun, Jun 25, 2023, 8:53 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -9,18 +9,36 @@ int main(void)
     int change = get_int("Change owed: ");
 
     // Count number of coins with n
-    int n = 0
+    int n = 0;
 
     // Subtract 25 cents until < 25 cents remaining, increment counter accordingly
-    while ()
+    while (change > 24)
+    {
+        change -= 25;
+        n ++;
+    }
 
     // Subtract 10 cents until < 10 cents remaining, increment counter accordingly
-
+    while (change > 9)
+    {
+        change -= 10;
+        n ++;
+    }
 
     // Subtract 5 cents until < 5 cents remaining, increment counter accordingly
-
+    while (change > 4)
+    {
+        change -= 5;
+        n ++;
+    }
 
     // Subtract 1 cent until 0 cents remaining, increment counter accordingly
+    while (change > 0)
+    {
+        change -= 1;
+        n ++;
+    }
 
-
+    // Print final number of coins
+    printf("%i\n", n);
 }
