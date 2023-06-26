e7eea5403ded9c0cb6ecf97882738b305c940c57
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 21:01:30 +0000

    Mon, Jun 26, 2023, 5:01 PM -04:00

--- week2/lecture/scores.c
+++ week2/lecture/scores.c
@@ -4,9 +4,11 @@
 int main(void)
 {
     int scores[3];
-    scores[0] = get_int("Score 1: ");
-    scores[1] = get_int("Score 2: ");
-    scores[2] = get_int("Score 3: ");
+
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score %i: ", i + 1);
+    }
 
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 
