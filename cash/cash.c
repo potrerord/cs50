a5dfec357e2d9c5688fbd508445dc249d99985f4
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 01:05:46 +0000

    Sun, Jun 25, 2023, 9:05 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -59,7 +59,10 @@ int get_change(void)
 }
 
 // Subtract largest coin possible
-void subtract_coin(total)
+void subtract_coin(int total, int coin)
 {
-    
+    while total >= coin
+    {
+        
+    }
 }
