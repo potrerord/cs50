3d0188386cea846dd77fc658e35e540e3faf9fa2
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 14:37:30 +0000

    Mon, Jun 26, 2023, 10:37 AM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,9 +3,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Gets valid credit card number from user input
+long get_long(long cc);
+
 int main(void)
 {
     // Prompt user for cc number
+    long cc = get_long("Number: ");
+
+    //
 
 
+}
+
+// Gets valid credit card number from user input
+long get_long(long cc);
+{
+    long cc;
+    do
+    {
+
+    }
+    while ()
 }
