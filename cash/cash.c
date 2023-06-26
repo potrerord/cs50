385e3c627a00b6a16267b3a5703aefbaa04447a3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 01:30:52 +0000

    Sun, Jun 25, 2023, 9:30 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -14,11 +14,11 @@ int main(void)
     // Count number of coins with n
     int n = 0;
 
-    // Subtract largest available coin and increment n by 1
+    // Subtract largest possible coin and increment n by 1, starting with quarter
     int coin = 25
     while (change > 0)
     {
-        // Compare change value to highest coin value
+        // Compare change value to highest coin value and subtract/increment if possible
         if (change >= coin)
         {
             change -= coin;
@@ -26,7 +26,7 @@ int main(void)
         }
         else
         {
-            // Lower coin value to next highest
+            // Lower coin value to next highest and loop until change is 0
             if (coin == 25)
             {
                 coin = 10;
