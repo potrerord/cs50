630244b949ddd41844ae3853d7b3f215de3b6407
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-26 14:34:23 +0000

    Mon, Jun 26, 2023, 10:34 AM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,11 @@
+// Prompts user for credit card number and reports whether it is a valid American Express, MasterCard, or Visa
+
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // Prompt user for cc number
+
+
 }
