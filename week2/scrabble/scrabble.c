fd57c3db6e96117b529c1b94e689db4533c54077
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-27 21:02:31 +0000

    Tue, Jun 27, 2023, 5:02 PM -04:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 // Scrabble letter values alpha order
-const int SCRABVAL = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
+const int SCRABVAL[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
 
 // Gets scrabble score of input alpha characters
 int scrabble_score(string s);
@@ -47,30 +47,27 @@ int main(void)
 // Gets scrabble score of input alpha characters
 int scrabble_score(string s)
 {
-    // Initialize final and letter-by-letter score variables, plus char x variable for s[i] to avoid changing str data
+    // Initialize final and letter-by-letter score variables, plus char x variable to store s[i]
     int sum = 0;
-    int letter = 0;
+    int xval = 0;
     char x;
 
     // For every character in s
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        // Char variable x for s[i] to avoid changing string data
         x = s[i];
 
         // If x is alpha
         if (isalpha(x) != 0)
         {
             // Convert x to uppercase if necessary
-            if (isupper(x) == 0)
-            {
-                x = toupper(x);
-            }
+            x = toupper(s[i]);
 
             // Check against the all-uppercase score key
-            // letter = whatever the score key says for that letter
+            xval = SCRABVAL[x - 65];
 
-            sum += letter;
+            // Add value to summed score
+            sum += xval;
         }
     }
     return sum;
