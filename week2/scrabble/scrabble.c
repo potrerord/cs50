c5962ea6a42bfec2b3b33bbad5cf4af6ee6dea7d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-27 22:02:40 +0000

    Tue, Jun 27, 2023, 6:02 PM -04:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -1,15 +1,15 @@
-// Takes 2 Scrabble word inputs, compares Player 1 to Player 2 and prints win result
+// Takes N Scrabble word inputs, prints winner/tie
 
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
-// Scrabble letter values alpha order
-const int SCRABVAL[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
 // Number of players
 const int N = 3;
+
+// Scrabble letter values alpha order
+const int SCRABVAL[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 // Returns 1 if all values are equal, 0 if any are different (requires an array input with null final value)
 int is_tie(int scores[]);
