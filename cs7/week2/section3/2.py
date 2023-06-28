002d0fbcdcbafbb7f00cac393b87a961bc4da0f7
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-28 22:24:34 +0000

    Wed, Jun 28, 2023, 6:24 PM -04:00

---
+++ cs7/week2/section3/2.py
@@ -0,0 +1,3 @@
+b = float(input("Enter triangle base:   "))
+h = float(input("Enter triangle height: "))
+print("Area is ", 0.5 * b * h)
---
+++ cs7/week2/section3/3.py
@@ -0,0 +1,5 @@
+n = int(input("How many la do you want? "))
+print("Fa ")
+for i in range(n):
+    print("la ")
+print("LA!")
