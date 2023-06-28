d1f6848d7081b3d0c956f33f3715cac84668209b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-28 16:07:42 +0000

    Wed, Jun 28, 2023, 12:07 PM -04:00

--- week2/section/reverse.c
+++ week2/section/reverse.c
@@ -1,3 +1,5 @@
+// Reverses a string
+
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -5,9 +7,18 @@
 int main(void)
 {
     string s = get_string("Enter string: ");
+    int length = strlen(s);
+    char array[length];
+    int n = 0;
 
-    
-
-    char array[strlen(s)]
-
+    for (int i = 0; i < length; i++)
+    {
+        n = length - i - 1;
+        array[n] = s[i];
+    }
+    for (int i = 0; i < length ; i++)
+    {
+        printf("%c", array[i]);
+    }
+    printf("\n");
 }
