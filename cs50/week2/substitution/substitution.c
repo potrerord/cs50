b2d74e55312b81a1061c336759590702c31a2f0b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-28 23:27:37 +0000

    Wed, Jun 28, 2023, 7:27 PM -04:00

--- cs50/week2/substitution/substitution.c
+++ cs50/week2/substitution/substitution.c
@@ -22,14 +22,14 @@ int main(int argc, string argv[])
     else
     {
         // Error if argument is not proper key (26 chars, all alpha, all 26 letters exactly once)
+
         int j = 0;
         while (argv[1][j] != '\0')
         {
-            if (isdigit(argv[1][j]) == 0)
+            if (isdigit(argv[1][i]) == 0)
             {
                 printf("Usage: ./caesar key\n");
                 return 1;
-                break;
             }
             j++;
         }
