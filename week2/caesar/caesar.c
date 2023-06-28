0db1498c55a3c9988c0f13bd44df8f0d707478ab
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-28 18:35:36 +0000

    Wed, Jun 28, 2023, 2:35 PM -04:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -34,12 +34,11 @@ int main(int argc, string argv[])
 
         // If argument is nonnegative int, continue to main function
 
-        
-        // (Recall that an int can overflow.) But, even if k is greater than 26,
-        // alphabetical characters in your program’s input should remain alphabetical characters
-        // in your program’s output. For instance, if k is 27, A should not become \ even though \ is 27
-        // positions away from A in ASCII, per asciitable.com; A should become B, since B is 27 positions
-        // away from A, provided you wrap around from Z to A.
+        // Even if k is greater than 26, alphabetical characters in your program’s input
+        // should remain alphabetical characters in your program’s output.
+        // For instance, if k is 27, A should not become \ even though \ is 27 positions away from
+        // A in ASCII,per asciitable.com; A should become B, since B is 27 positions away from A,
+        //  provided you wrap around from Z to A.
 
         // Your program must output plaintext: (with two spaces but without a newline)
         // and then prompt the user for a string of plaintext (using get_string).
