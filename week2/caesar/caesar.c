6c922a76865265a7e01e4718bf3eabfa74bd4bdf
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-28 19:00:26 +0000

    Wed, Jun 28, 2023, 3:00 PM -04:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -3,6 +3,7 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
@@ -17,7 +18,7 @@ int main(int argc, string argv[])
     // If single argument
     else
     {
-        // Error if argument is not nonnegative int
+        // Error if argument is not nonnegative int, otherwise continue through main
         for (int i = 0; 1 == 1; i++)
         {
             if (argv[1][i] == '\0')
@@ -32,32 +33,16 @@ int main(int argc, string argv[])
             }
         }
 
-        // If argument is nonnegative int, continue to main function
-
-        // Even if k is greater than 26, alphabetical characters in your program’s input
-        // should remain alphabetical characters in your program’s output.
-        // For instance, if k is 27, A should not become \ even though \ is 27 positions away from
-        // A in ASCII,per asciitable.com; A should become B, since B is 27 positions away from A,
-        // provided you wrap around from Z to A.
-
         // Prompt user for plaintext string
         string plaintext = get_string("plaintext:  ");
-
-        // Rotate alphabet according to key, ignoring nonalpha chars
-
 
         // Print encrypted ciphertext
         printf("ciphertext: ");
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
-            printf('%c', ciphertext[i] + (int) (argv) % 26);
+            printf('%c', (ciphertext[i] + atoi(argv));
         }
         printf("\n");
-
-        // Your program must preserve case: capitalized letters, though rotated, must remain
-        // capitalized letters; lowercase letters, though rotated, must remain lowercase letters.
-
-        // After outputting ciphertext, you should print a newline.
-        // Your program should then exit by returning 0 from main.
+        return 0;
     }
 }
