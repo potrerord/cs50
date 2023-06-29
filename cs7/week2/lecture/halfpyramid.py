8e09db606c375349e4112428f91754a0e97770ac
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 19:28:22 +0000

    Thu, Jun 29, 2023, 3:28 PM -04:00

--- cs7/week2/lecture/halfpyramid.py
+++ cs7/week2/lecture/halfpyramid.py
@@ -1,2 +1,8 @@
+## Prints a mario half pyramid of user-input size
+
 size = int(input("Enter size: "))
 
+for i in range (size):
+    for j in range (size - 1)):
+        print(" ", end = "")
+    for j in range (
