bcef78874bc7782375834e1c2bc6a03e571a287c
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 15:01:46 +0000

    Thu, Jun 29, 2023, 11:01 AM -04:00

---
+++ cs7/week2/pset/partA/2.py
@@ -0,0 +1,2 @@
+for count in range(6):
+    print (15 * count + 4)
---
+++ cs7/week2/pset/partA/3.py
@@ -0,0 +1,9 @@
+def main():
+    print ("She said, \"", end="")
+    print ("Never put off till tomorrow... [notice the space]", end="")
+    print ("\nWhat you can do")
+    print ("The day", end="")
+    print ()
+    print ("After tomorrow!\"")
+
+main()
