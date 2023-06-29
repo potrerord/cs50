7e8e1113dd57a8895d6d817fa1ef81085e4f3b4b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 19:43:48 +0000

    Thu, Jun 29, 2023, 3:43 PM -04:00

--- cs7/week2/lecture/mario2.py
+++ cs7/week2/lecture/mario2.py
@@ -6,11 +6,11 @@ n = int(input("Enter size: "))
 ## Print n rows
 for i in range (1, n + 1):
 
-    ## Print n spaces
-    print(' ' * n)
+    ## Print decreasing spaces
+    print(' ' * (n - i), end = '')
 
-    ## Print # - number matches row number
-    print('#' * i)
+    ## Print increasing # - number matches row number
+    print('#' * i, end = '')
 
     ## Single newline at end of each row
     print()
