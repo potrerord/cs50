d4cef718526d87aa78c90aee122ae58cbe264887
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 21:10:36 +0000

    Thu, Jun 29, 2023, 5:10 PM -04:00

---
+++ cs7/week2/class4/lecture4/integers.py
@@ -0,0 +1,9 @@
+# I have been told that there are a few 3-digit integers that are equal to the sum of the cube of their individual digits. Find all of them, and print them out!
+
+n = 0
+while (n < 100 or n >999):
+    n = int(input("Enter a 3-digit integer: "))
+
+a = n % 100
+b = n % 10
+c = n % 
