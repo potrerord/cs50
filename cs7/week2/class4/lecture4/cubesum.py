4ea63baa44ccd89dabfff4cb37f18c9b6c4d1a52
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 21:36:23 +0000

    Thu, Jun 29, 2023, 5:36 PM -04:00

--- cs7/week2/class4/lecture4/cubesum.py
+++ cs7/week2/class4/lecture4/cubesum.py
@@ -18,3 +18,20 @@ for n in range(100, 1000):
 
     # Increment to next 3 digit integer
     n += 1
+
+"""
+# Attempt 1
+
+# Start at 100, go to 999
+for n in range(100, 1000):
+    a = n // 100 % 10
+    b = n // 10 % 10
+    c = n % 10
+
+    # Print if sum of digits' cubes equals n
+    if n == a**3 + b**3 + c**3:
+        print(n)
+
+    # Increment to next 3 digit integer
+    n += 1
+"""
