fd8220ed670f94e5d41b8314441f538b46232b93
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 20:48:06 +0000

    Thu, Jun 29, 2023, 4:48 PM -04:00

--- cs7/week2/class4/lecture4/discriminant.py
+++ cs7/week2/class4/lecture4/discriminant.py
@@ -1,7 +1,21 @@
+# Calculates if the discriminant of a quadratic equation is negative
+
+"""
+# Naive way
+
+# User prompt
 a = int(input("a = "))
 b = int(input("b = "))
 c = int(input("c = "))
 
+# Store boolean value for whether the discriminant is a negative number
 discr = b ** 2 - (4 * a * c) < 0
 print(discr)
+"""
 
+# User prompt
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+# etc etc lecture's moving on lol
---
+++ cs7/week2/class4/lecture4/lettergrade.py
@@ -0,0 +1,6 @@
+# Converts numerical exam score to letter grade
+
+# User input
+grade = int(input("Please type a student's numerical exam score: "))
+
+if grade 
