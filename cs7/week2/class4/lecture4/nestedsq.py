c5caf951d4837ea8ceb1f15a6160b7c158207fa1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 21:01:35 +0000

    Thu, Jun 29, 2023, 5:01 PM -04:00

---
+++ cs7/week2/class4/lecture4/nestedsq.py
@@ -0,0 +1,14 @@
+# Prints nested squares out of * and . of size n x n
+
+# Prompt user
+n = int(input("Enter size: "))
+
+# Print n rows; counter variable is 1 for usage in calculations
+for row in range (1, n + 1):
+    # Every row is the same
+
+    # Even: *, Odd: .
+
+    # First & last row is always all one character
+
+    # First + 1 / Last - 1 is always mostly the other character, with the first character at the ends
