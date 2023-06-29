2e94b29cf3ce3c4cd0ff8f10eafb8216bba300d1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 19:40:55 +0000

    Thu, Jun 29, 2023, 3:40 PM -04:00

--- cs7/week2/lecture/mario2.py
+++ cs7/week2/lecture/mario2.py
@@ -1,8 +1,12 @@
 ## Prints mario half pyramid in a Pythonic way
 
+## Prompt user for size of half pyramid
 n = int(input("Enter size: "))
 
+## Print n rows
 for i in range (1, n + 1):
-    print(' ' * (n))
+
+    ## Print n spaces
+    print(' ' * n)
     print('#' * i)
     print()
