cd7f6773cce5ca3802059d9094df7888a46f29a1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 22:25:03 +0000

    Thu, Jun 29, 2023, 6:25 PM -04:00

--- cs7/week2/pset2/pset2b/easter.py
+++ cs7/week2/pset2/pset2b/easter.py
@@ -1,2 +1,20 @@
 # Computes the date of Easter Sunday in input year.
 
+y = int(input("Enter a year: "))
+
+# The variables below correspond with the Gauss Easter calculation formula.
+a = y % 19
+b = y // 100
+c = y % 100
+d = b // 4
+e = b % 4
+g = (8 * b + 13) // 25
+h = (19 * a + b - d - g + 15) % 30
+j = c // 4
+k = c % 4
+m = a + 11 * h) // 319
+r = (2 * e + 2 * j - k - h + m + 32) % 7
+n = (h - m + r + 90) // 25
+p = (h - m + t + n + 19) % 32
+
+print()
