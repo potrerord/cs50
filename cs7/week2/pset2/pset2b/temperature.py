097b3d6f9653117273db1789924551d1c875109f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 21:52:07 +0000

    Thu, Jun 29, 2023, 5:52 PM -04:00

--- cs7/week2/pset2/pset2b/temperature.py
+++ cs7/week2/pset2/pset2b/temperature.py
@@ -1,2 +1,7 @@
 # Prints the Kelvin equivalent of any temperature in degrees Fahrenheit
 
+fahrenheit = float(input("Input a temperature in degrees Fahrenheit to be converted: "))
+
+kelvin = (5 / 9) * (fahrenheit - 32) + 273.16
+
+print(f"{fahrenheit} degrees Fahrenheit equals {kelvin} Kelvins.")
