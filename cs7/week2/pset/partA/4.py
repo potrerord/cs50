c9804a1bb17ad66c48e5c16c9237ad1ba4bc0b84
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-29 15:08:27 +0000

    Thu, Jun 29, 2023, 11:08 AM -04:00

---
+++ cs7/week2/pset/partA/4.py
@@ -0,0 +1,8 @@
+first = 18
+second = -9
+first += second
+second = first - second
+first = first - second
+
+print ("\nFirst is", first)
+print ("Second is", second, "\n")
---
+++ cs7/week2/pset/partA/5.py
@@ -0,0 +1,17 @@
+def first():
+    print ("Inside first function!")
+
+def second():
+    first()
+    print ("Inside second function!")
+
+def third():
+    print ("Inside third function!")
+    first()
+    second()
+
+def main():
+    first()
+    third()
+    second()
+    third()
