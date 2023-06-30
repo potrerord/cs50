f4dfacd4637c0c370c7810d62b9017ade24f0463
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 21:38:23 +0000

    Fri, Jun 30, 2023, 5:38 PM -04:00

--- cs7/week2/class4/section4/restaurant.py
+++ cs7/week2/class4/section4/restaurant.py
@@ -5,7 +5,13 @@ NAME = "Devon"
 
 def main():
     print(f"Hi, {NAME}!")
-    order = get_size("What would you like to order? ")
+
+    # Get order until customer stops.
+    while True:
+        order = get_size("What would you like to order? ")
+        "Would you like to add another item? (y/n)"
+
+    # Did you mean _________?
 
 
 def get_alpha(prompt: str) -> str:
@@ -21,6 +27,24 @@ def get_alpha(prompt: str) -> str:
         if user_input.isalpha():
             return float(user_input)
         print("I'm sorry, I didn't quite get that.")
+
+
+def get_redo() -> bool:
+    """Prompt user to loop program if they wish, with y/n input.
+
+    Reprompt user if input is not 'y' or 'n' (case agnostic).
+    Return boolean value.
+    """
+    while True:
+        user_response = input("\nWould you like to print another "
+                            "box? (y/n): ")
+        if user_response.lower() == "n":
+            return False
+        elif user_response.lower() != "y":
+            print("Sorry, didn't get that - please respond with "
+                "'y' or 'n'.")
+        else:
+            return True
 
 
 main()
