a9a7ce02f95fe33f790a248607d246746491abd3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 16:14:35 +0000

    Fri, Jun 30, 2023, 12:14 PM -04:00

--- cs7/week2/class3/lecture/box.py
+++ cs7/week2/class3/lecture/box.py
@@ -2,3 +2,7 @@
 
 size = int(input("Enter a size: "))
 
+for row in range()
+# First row always prints +, then n -, then +
+# Middle rows always print |, then n " ", then |
+# Last row prints first row
