0080a5fc03bef71ffa70e318dc41766af8d6ff47
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 16:29:53 +0000

    Fri, Jun 30, 2023, 12:29 PM -04:00

--- cs7/week2/class3/lecture/box.py
+++ cs7/week2/class3/lecture/box.py
@@ -4,10 +4,35 @@ size = int(input("Enter a size: "))
 
 # Range starts at 1 to use row number in calculations.
 for row in range(1, size + 1):
-    if (row = 1):
+
+    # Define end/middle characters depending on row.
+    if(row == 1 or row == size):
+        end_char = "+"
+        mid_char = "-"
+    else:
+        end_char = "|"
+        mid_char = "|"
+
+    print(end_char)
+
+    # Print first character.
+
+
+    # Print middle characters.
+
+
+    # Print last character.
+
+    # Prints +--+ for first/last rows.
+    if(row == 1 or row == size):
         print("+", end="")
-        print("-" * (size - 1)
+        print("-" * (size - 1), end="")
         print("+")
+
+    # Prints |  | for middle rows.
+    else:
+        print("|", end="")
+        print(" " )
 # First row always prints +, then n -, then +
 # Middle rows always print |, then n " ", then |
 # Last row prints first row
