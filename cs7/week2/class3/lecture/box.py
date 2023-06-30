4ab3d8f14911ecfe252741d09e881bda12d21791
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 16:22:37 +0000

    Fri, Jun 30, 2023, 12:22 PM -04:00

--- cs7/week2/class3/lecture/box.py
+++ cs7/week2/class3/lecture/box.py
@@ -5,6 +5,8 @@ size = int(input("Enter a size: "))
 # Range starts at 1 to use row number in calculations.
 for row in range(1, size + 1):
     if (row = 1):
+        print("+", end="")
+        print("-" * (size - 1)
         print("+")
 # First row always prints +, then n -, then +
 # Middle rows always print |, then n " ", then |
