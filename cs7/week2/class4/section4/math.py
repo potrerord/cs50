a478d0a512d1e6b0bdadc9dfd662cd63be1d6e9d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 22:02:08 +0000

    Fri, Jun 30, 2023, 6:02 PM -04:00

--- cs7/week2/class4/section4/math.py
+++ cs7/week2/class4/section4/math.py
@@ -6,17 +6,23 @@ def main():
 
 
 
-def quad_formula_pos(a, b, c, ) -> float:
+def quad_formula_pos(a, b, c) -> float:
     """Solves a quadratic equation with constants a, b, and c, then
     returns the output.
 
     arguments:
     a -- leading coefficient (quadratic term)
-    b -- middle coefiicient (linear term)
+    b -- middle coefficient (linear term)
     c -- constant term
     """
 
-    x = -b + math.sqrt(b)
+    disc = b ** 2 - 4 * a * c
+
+    # If discriminant is <= 0, cut it
+    if disc <= 0:
+        
+
+    x = -b + math.sqrt(b ** 2 - 4 * a * c) / (2 * a)
 
 
 
