cff18e338030787e6970e73cbca53163e92d1138
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 20:50:37 +0000

    Fri, Jun 30, 2023, 4:50 PM -04:00

--- cs7/week2/pset2/pset2b/oops.py
+++ cs7/week2/pset2/pset2b/oops.py
@@ -7,8 +7,9 @@ the class. Uses a ratio of 1 TA for every 15 students, rounded down.
 
 def main():
     """Prompt and calculate."""
+
     studs = int(input("How many students are enrolled? "))
-    print ("We need", studs // 15, "teaching assistants")
+    print("We need", studs // 15, "teaching assistants.")
 
 
 main()
