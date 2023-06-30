b54423afded6cc20432a94f128c2906c46d19c10
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 21:52:17 +0000

    Fri, Jun 30, 2023, 5:52 PM -04:00

---
+++ cs7/week2/class4/section4/math.py
@@ -0,0 +1,22 @@
+"""Calculates using some formula."""
+
+
+def main():
+
+
+
+def quad_formula(a, b, c) -> float:
+    """Solves a quadratic equation with constants a, b, and c, then
+    returns the output.
+
+    arguments:
+    a -- leading coefficient (quadratic term)
+    b -- middle coefiicient (linear term)
+    c -- constant term
+    """
+
+
+
+
+
+main()
