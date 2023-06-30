d9c75a138ace22a317bff9e23e6bac897a90e52a
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 19:51:32 +0000

    Fri, Jun 30, 2023, 3:51 PM -04:00

--- cs7/week2/pset2/pset2b/evens.py
+++ cs7/week2/pset2/pset2b/evens.py
@@ -8,11 +8,23 @@ def main():
     sequence and print sum.
     """
 
-    n = int(input("I will compute the sum of even integers from "
-                  "2 through? "))
-    sum = 0
+    n = get_range()
 
+    sum = 0
     for even in range(2, n + 1, 2):
         sum += even
 
     print("The sum is", sum)
+
+
+def get_range() -> int:
+    """Prompt user and continually reprompt if input is not a positive
+    integer greater than 2. Return integer input.
+    """
+
+    while True:
+        user_input = input("\nI will compute the sum of even integers "
+                           "from 2 through? ")
+        if user_input.isdigit() and int(user_input) > 0:
+            return int(user_input)
+        print("Size must be a positive integer greater than 2.")
