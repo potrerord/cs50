dd275159ed8479ea0bb16831059ea1ac828be426
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 23:09:26 +0000

    Fri, Jun 30, 2023, 7:09 PM -04:00

--- cs7/week2/class3/lecture3/led.py
+++ cs7/week2/class3/lecture3/led.py
@@ -1,20 +1,51 @@
 """Prints an LED-style display of the digits 1-9."""
 
 def main():
-    print_led(1, 1, 1, 1, 1, 1, 1,)
+    """Print digits according to manually-coded on/off codes.
+
+    Forever loop for testing purposes.
+    """
+
+    while True:
+        digit = int(input("Enter digit: "))
+
+        if digit == 0:
+            print_led(1, 1, 1, 0, 1, 1, 1)
+        elif digit == 1:
+            print_led(0, 0, 1, 0, 0, 1, 0)
+        elif digit == 2:
+            print_led(1, 0, 1, 1, 1, 0, 1)
+        elif digit == 3:
+            print_led(1, 0, 1, 1, 0, 1, 1)
+        elif digit == 4:
+            print_led(0, 1, 1, 1, 0, 1, 0)
+        elif digit == 5:
+            print_led(1, 1, 0, 1, 0, 1, 1)
+        elif digit == 6:
+            print_led(1, 1, 0, 1, 1, 1, 1)
+        elif digit == 7:
+            print_led(1, 0, 1, 0, 0, 1, 0)
+        elif digit == 8:
+            print_led(1, 1, 1, 1, 1, 1, 1)
+        elif digit == 9:
+            print_led(1, 1, 1, 1, 0, 1, 1)
 
 
 def print_led(top, topleft, topright, middle, bottomleft,
               bottomright, bottom):
-    """Print the LED version of the digit provided as an argument."""
+    """Print the LED version of the digit provided as an argument.
 
-    # Top:
+    Arguments designate whether LED segment is "on" with 1 or "off" with
+    2.
+    """
+
+    # Top
     if top == 1:
         print(" ----")
     else:
         print()
 
-    # Top left/right:
+    # Top left/right
     for i in range(2):
         if topleft == 1:
             print("|", end="")
@@ -25,11 +56,13 @@ def print_led(top, topleft, topright, middle, bottomleft,
         else:
             print()
 
-    # Middle: "----" or "    "
+    # Middle
     if middle == 1:
         print(" ----")
+    else:
+        print()
 
-    # Bottom left/right:
+    # Bottom left/right
     for i in range(2):
         if bottomleft == 1:
             print("|", end="")
@@ -40,9 +73,11 @@ def print_led(top, topleft, topright, middle, bottomleft,
         else:
             print()
 
-    # Bottom: "----" or "    "
+    # Bottom
     if bottom == 1:
         print(" ----")
+    else:
+        print()
 
 
 main()
