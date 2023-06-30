e9876a3c233df2f8b1080612912db045959c9571
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 22:50:02 +0000

    Fri, Jun 30, 2023, 6:50 PM -04:00

--- cs7/week2/class3/lecture3/led.py
+++ cs7/week2/class3/lecture3/led.py
@@ -1,13 +1,29 @@
 """Prints an LED-style display of the digits 1-9."""
 
 def main():
+    print_led()
 
 
-
-def print_led(digit: int):
+def print_led():
     """Print the LED version of the digit provided as an argument."""
 
-    
+    # Top:
+    print(" ", end="")
+    print("----")
+
+    # Top left/right:
+    print("|\n|")
+
+    # Middle: "----" or "    "
+    print(" ", end="")
+    print("----")
+
+    # Bottom left/right:
+    print("|\n|")
+
+    # Bottom: "----" or "    "
+    print(" ", end="")
+    print("----")
 
 
 main()
