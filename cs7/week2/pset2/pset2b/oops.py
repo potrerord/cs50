97aad245d476f1b3c0193f7345b5b06cc8701fc8
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 20:48:57 +0000

    Fri, Jun 30, 2023, 4:48 PM -04:00

--- cs7/week2/pset2/pset2b/oops.py
+++ cs7/week2/pset2/pset2b/oops.py
@@ -1,9 +1,14 @@
 """
-Calculates and prints the number of teaching fellows s necessary for input students (1 TA per 15 students).
+Prompts user for a number of students enrolled in a class, then
+calculates and prints the number of teaching assistants necessary for
+the class. Uses a ratio of 1 TA for every 15 students, rounded down.
 """
 
+
 def main():
+    """Prompt and calculate."""
     studs = int(input("How many students are enrolled? "))
     print ("We need", studs // 15, "teaching assistants")
+
 
 main()
