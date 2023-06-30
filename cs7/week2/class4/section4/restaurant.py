ede2967aa004ed4b29aa53c555a714e51bd639fd
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 21:29:41 +0000

    Fri, Jun 30, 2023, 5:29 PM -04:00

--- cs7/week2/class4/section4/restaurant.py
+++ cs7/week2/class4/section4/restaurant.py
@@ -1,8 +1,41 @@
 """Takes your order."""
 
+NAME = "Devon"
+
 def main():
-    print("Hello")
-    print("What would you like to order idr the assignment lmao")
+    print("Hello, {NAME
+    order = get_order("What would you like to order? ")
+
+
+
+def get_order(prompt: str) -> str:
+    """Prompt user and continually reprompt if input is not alpha.
+    Return string user input.
+
+    Keyword argument:
+    prompt -- The string prompt that will accept user input.
+    """
+
+    while True:
+        user_input = input("\n" + prompt)
+        if user_input.isdigit():
+            return float(user_input)
+        print("error: temperature must be numeric")
+
+
+def get_name(prompt: str) -> float:
+    """Prompt user and continually reprompt if input is alpha.
+    Return float user input.
+
+    Keyword argument:
+    prompt -- The string prompt that will accept user input.
+    """
+
+    while True:
+        user_input = input("\n" + prompt)
+        if user_input.isdigit():
+            return float(user_input)
+        print("error: temperature must be numeric")
 
 
 main()
