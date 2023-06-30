92e60b10224b7b17fe3877f0027c5575b5730d4f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-06-30 16:51:56 +0000

    Fri, Jun 30, 2023, 12:51 PM -04:00

--- cs7/week2/class3/lecture/box.py
+++ cs7/week2/class3/lecture/box.py
@@ -4,19 +4,19 @@ size = int(input("Enter a size: "))
 
 # Range starts at 1 to use row number in calculations.
 
-def draw_box():
-for row in range(1, size + 1):
+def draw_box(size):
+    for row in range(1, size + 1):
 
-    # Define end/middle "characters" depending on row. mid_char uses two
-    # of each character to more closely approximate the height of a line
-    # of text.
-    if(row == 1 or row == size):
-        end_char = "+"
-        mid_char = "--"
-    else:
-        end_char = "|"
-        mid_char = "  "
+        # Define end/middle "characters" depending on row. mid_char uses
+        # two of each character to more closely approximate the height
+        # of a line of text.
+        if(row == 1 or row == size):
+            end_char = "+"
+            mid_char = "--"
+        else:
+            end_char = "|"
+            mid_char = "  "
 
-    # Prints end, middle, end for each row. Middle is reduced by 2 to
-    # account for end characters.
-    print(end_char + mid_char * (size - 2) + end_char)
+        # Prints end, middle, end for each row. Middle is reduced by 2
+        # to account for end characters.
+        print(end_char + mid_char * (size - 2) + end_char)
