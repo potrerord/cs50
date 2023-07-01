c2afe15a0b8a7053ab0dd3c67efb92f04cc0d9e8
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-01 20:39:46 +0000

    Sat, Jul 1, 2023, 4:39 PM -04:00

---
+++ cs50/week3/lecture3/mario_rec2.c
@@ -0,0 +1,36 @@
+// Recursively prints a mario pyramid (upside down though :/).
+
+#include <cs50.h>
+#include <stdio.h>
+
+// Prototypes:
+int print_pyr(int n);
+
+
+// Call the recursive print_pyr function.
+int main(void)
+{
+    int height = get_int("Enter height: ");
+    print_pyr(height);
+}
+
+
+// Recursively print n rows with base case n = #.
+int print_pyr(int n)
+{
+    // Base case: Print one block with n - 1 spaces in front.
+    if (n == 1)
+    {
+        printf("#\n");
+        return 1;
+    }
+
+    // Recursive case: Print previous row with 1 less space and one more
+    // block. End if no more spaces / all blocks.
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    return print_pyr(n - 1);
+}
