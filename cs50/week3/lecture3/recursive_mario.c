701fdcd3917203cf18e4a5289b3608fbda759f92
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-01 20:29:48 +0000

    Sat, Jul 1, 2023, 4:29 PM -04:00

--- cs50/week3/lecture3/recursive_mario.c
+++ cs50/week3/lecture3/recursive_mario.c
@@ -1,17 +1,35 @@
+// Recursively prints a mario pyramid.
+
 #include <cs50.h>
 #include <stdio.h>
 
+// Prototypes:
+int print_pyr(int n);
+
+
+// Call the recursive print_pyr function.
 int main(void)
 {
-
+    int height = get_int("Enter height: ");
+    print_pyr(height);
 }
 
 
 // Recursively print n rows with base case n = #.
-void print_pyr(int n)
+int print_pyr(int n)
 {
+    // Base case: Print one block.
     if (n == 1)
     {
-        
+        printf("#\n");
+        return 1;
     }
+
+    // Recursive case: Print n blocks with a new line.
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    return print_pyr(n - 1);
 }
