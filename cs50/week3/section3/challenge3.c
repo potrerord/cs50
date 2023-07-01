81c5723b3e5a8a88256ac8a5b9fac6dd61ac71c2
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-01 15:37:47 +0000

    Sat, Jul 1, 2023, 11:37 AM -04:00

--- cs50/week3/section3/challenge3.c
+++ cs50/week3/section3/challenge3.c
@@ -1,6 +1,21 @@
 // Create a struct that one might use to track info about a student in a gradebook
-
 // Write a program that defines the name, stud id and gpa of three students
-
 // Then print
 
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string id;
+    float gpa;
+}
+student;
+
+
+int main(void)
+{
+    
+}
