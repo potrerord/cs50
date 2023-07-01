d753a974f430bbe58f2442fe14fb5c7605650d79
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-01 21:06:22 +0000

    Sat, Jul 1, 2023, 5:06 PM -04:00

--- cs50/week3/lecture3/mario_rec2.c
+++ cs50/week3/lecture3/mario_rec2.c
@@ -4,20 +4,20 @@
 #include <stdio.h>
 
 // Prototypes:
-int draw(int n);
+void draw(int blocks, int spaces);
 
 
 // Call the recursive print_pyr function.
 int main(void)
 {
     int height = get_int("Enter height: ");
-    draw(height);
+    draw(height, 0);
 }
 
 
 // Recursively print n rows with base case n = space.
 // Ideally, figure out a way for default spaces to be 0.
-int draw(int blocks, int spaces)
+void draw(int blocks, int spaces)
 {
     // Base case: Print n - 1 blocks.
     if (blocks <= 1)
@@ -33,9 +33,14 @@ int draw(int blocks, int spaces)
     draw(blocks - 1, spaces + 1);
 
     // Recursive case: Print n - 1 spaces.
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < spaces; i++)
     {
-        printf("-");
+        printf(" ");
+    }
+    for (int i = 0; i < blocks; i++)
+    {
+        printf("#");
     }
     printf("\n");
+    return
 }
