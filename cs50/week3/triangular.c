1077cec973c5bc97e254cbdb9e70bb010d5418be
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-01 19:35:20 +0000

    Sat, Jul 1, 2023, 3:35 PM -04:00

---
+++ cs50/week3/triangular.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+// Prototypes
+int triangular(int n);
+
+
+int main(void)
+{
+    int user_n = get_int("Enter positive int: ");
+    printf("%i", triangular(user_n));
+}
+
+
+// Recursively calculate the nth triangular number.
+int triangular(int n)
+{
+    // Base case: the first triangular number is 1.
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    // Add n to the previous triangular number.
+    n = n + triangular(n);
+    return n;
+}
