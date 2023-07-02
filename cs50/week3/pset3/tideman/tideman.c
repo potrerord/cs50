064c367d5600f8364045d98dc2d61e8a76d7ec11
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-02 18:37:31 +0000

    Sun, Jul 2, 2023, 2:37 PM -04:00

--- cs50/week3/pset3/tideman/tideman.c
+++ cs50/week3/pset3/tideman/tideman.c
@@ -99,41 +99,116 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    // The function takes arguments rank, name, and ranks.
+
+
+
+    // If name is a match for the name of a valid candidate,
+    // then you should update the ranks array to indicate that the voter
+    // has the candidate as their rank preference (where 0 is the first
+    // preference, 1 is the second preference, etc.)
+
+
+
+    // Recall that ranks[i] here represents the user’s ith preference.
+
+
+
+    // The function should return true if the rank was successfully
+    // recorded, and false otherwise (if, for instance, name is not the
+    // name of one of the candidates).
+
+
+
+    // You may assume that no two candidates will have the same name.
+
+
+
     return false;
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    // The function is called once for each voter, and takes as argument
+    // the ranks array, (recall that ranks[i] is the voter’s ith
+    // preference, where ranks[0] is the first preference).
+
+
+
+    // The function should update the global preferences array to add
+    // the current voter’s preferences. Recall that preferences[i][j]
+    // should represent the number of voters who prefer candidate i over
+    // candidate j.
+
+
+
+    // You may assume that every voter will rank each of the candidates.
+
+
+
     return;
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    // The function should add all pairs of candidates where one
+    // candidate is preferred to the pairs array.
+
+
+
+    // A pair of candidates who are tied (one is not preferred over the
+    // other) should not be added to the array.
+
+
+
+    // The function should update the global variable pair_count to be
+    // the number of pairs of candidates. (The pairs should thus all be
+    // stored between pairs[0] and pairs[pair_count - 1], inclusive).
+
+
+
     return;
 }
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    // The function should sort the pairs array in decreasing order of
+    // strength of victory, where strength of victory is defined to be
+    // the number of voters who prefer the preferred candidate.
+
+
+
+    // If multiple pairs have the same strength of victory, you may
+    // assume that the order does not matter.
+
+
+
     return;
 }
 
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    // The function should create the locked graph, adding all edges in
+    // decreasing order of victory strength so long as the edge would
+    // not create a cycle.
+
+
+
     return;
 }
 
 // Print the winner of the election
 void print_winner(void)
 {
-    // TODO
+    // The function should print out the name of the candidate who is
+    // the source of the graph. You may assume there will not be more
+    // than one source.
+
+    
+
     return;
 }
