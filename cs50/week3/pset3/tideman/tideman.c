3fd7bebc307c76c31e68bac3ae5183ea801e8a54
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-03 02:30:08 +0000

    Sun, Jul 2, 2023, 10:30 PM -04:00

--- cs50/week3/pset3/tideman/tideman.c
+++ cs50/week3/pset3/tideman/tideman.c
@@ -233,7 +233,7 @@ void sort_pairs(void)
         // accordingly.
         for (int j = i + 1; j < pair_count; j++)
         {
-            if (pairs[j].winner > pairs[largest_index].winner)
+            if (preferences[j] > pairs[largest_index].winner)
             {
                 largest_index = j;
             }
