f9a5d3c53c2cb1d7b6e87360a53c5982c850384a
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 01:43:58 +0000

    Mon, Jul 3, 2023, 9:43 PM -04:00

--- cs7/week2/pset2/pset2d/right_triangle.py
+++ cs7/week2/pset2/pset2d/right_triangle.py
@@ -11,24 +11,25 @@ def draw(stars: int, spaces: int):
     """Recursively print n rows with base case of no spaces and one
     star.
 
-    Default "spaces" value should be 0.
+    Default "stars" value should be 0.
     """
 
     # Base case: Print a row of all stars.
     if spaces < 1:
         for i in range(stars):
             print("*", end="")
+        print()
         return
 
     draw(stars + 1, spaces - 1)
 
     # Recursive case: Print previous row with one more space and one
     # fewer star.
-    print()
     for i in range(spaces):
         print(" ", end="")
     for i in range(stars):
         print("*", end="")
+    print()
     return
 
 main()
