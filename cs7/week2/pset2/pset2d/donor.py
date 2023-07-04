62053040b39d5b4a4bee51db99d17162963f958a
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 00:40:37 +0000

    Mon, Jul 3, 2023, 8:40 PM -04:00

---
+++ cs7/week2/pset2/pset2d/donor.py
@@ -0,0 +1,15 @@
+"""
+Classifies charity contributors as Benefactors, Patrons, Suppporters,
+Friends, or Cheapskates based on different donation tiers.
+"""
+
+
+def main():
+    user_donation = input("Enter the amount of a contribution: ")
+    print(donor(user_donation))
+
+def donor(donation:int):
+    if
+
+
+main()
