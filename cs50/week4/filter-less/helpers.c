45c3748c7eebcbf4e4d2b1b948a5450a5b72dc40
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 20:45:38 +0000

    Tue, Jul 4, 2023, 4:45 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -16,17 +16,28 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    // change every pixel to... a shade of brown? like gray but with red?
+
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // take every pixel, put into a buffer and then make a new file with the buffer where the width values are flipped?
+
+
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+
+
+
     return;
 }
