d94d9d7d92df827e0b5d37e3e98a1675ef26e05b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 23:14:58 +0000

    Tue, Jul 4, 2023, 7:14 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -89,27 +89,60 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    /*
-    // Iterate over all pixels in row i and column j.
-    for (int i = 0; i < height; i++)
+
+
+    // Iterate over all pixels in row px_row and column px_col.
+    for (int px_row = 0; px_row < height; px_row++)
     {
-        for (int j = 0; j < width; j++)
+        for (int px_col = 0; px_col < width; px_col++)
         {
-            // Declare variables to store R/G/B values for the pixel.
-            int red = image[i][j].rgbtRed;
-            int green = image[i][j].rgbtGreen;
-            int blue = image[i][j].rgbtBlue;
+            // Store R/G/B values for the pixel in orig[].
+            int orig[RGB_VALUES] = {
+                image[px_row][px_col].rgbtRed,
+                image[px_row][px_col].rgbtGreen,
+                image[px_row][px_col].rgbtBlue
+            };
 
-            //
 
+/*
+            // Store sepia conversion factors. Multiply each original
+            // R/G/B value by the factors in each row, respectively, to
+            // get that row's sepia value.
+            float conv_factors[RGB_VALUES][RGB_VALUES] = {
+                {0.393, 0.769, 0.189},  // Row 0: R; Columns: R, G, B
+                {0.349, 0.686, 0.168},  // Row 1: G; Columns: R, G, B
+                {0.272, 0.534, 0.131}   // Row 2: B; Columns: R, G, B
+            };
+
+            // Initialize all sepia values to 0.0 for conversion sum.
+            float sepia[RGB_VALUES] = {0.0};
+
+            // Convert to sepia using conv_factors row/col structure.
+            for (int rgb_row = 0; rgb_row < RGB_VALUES; rgb_row++)
+            {
+                for (int rgb_col = 0; rgb_col < RGB_VALUES; rgb_col++)
+                {
+                    // Compute sepia values by adding respective products.
+                    sepia[rgb_row] += (conv_factors[rgb_row][rgb_col] *
+                                       orig[rgb_col]);
+                }
+
+                // Cap each converted sepia value at 255.
+                if (sepia[rgb_row] > 255)
+                {
+                    sepia[rgb_row] = 255;
+                }
+            }
 
             // Update pixel values in array.
-            image[i][j].rgbtRed = ;
-            image[i][j].rgbtGreen = ;
-            image[i][j].rgbtBlue = ;
+            image[px_row][px_col].rgbtRed = sepia[0];
+            image[px_row][px_col].rgbtGreen = sepia[1];
+            image[px_row][px_col].rgbtBlue = sepia[2];
+
+*/
+
         }
     }
-    */
     return;
 }
 
