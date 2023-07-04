fbc34743501320407e9a4e9aa2b9a34fb20f306e
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 03:20:15 +0000

    Mon, Jul 3, 2023, 11:20 PM -04:00

--- cs7/week2/pset2/pset2e/perfect.py
+++ cs7/week2/pset2/pset2e/perfect.py
@@ -10,9 +10,21 @@ def main():
 
     for i in range(1, user_int + 1):
 
+    # Store the square root of the int (the square root of an integer is
+    # the upper limit for where you'll stop finding new factor pairs).
+    sqrt_user_int = user_int ** 0.5
 
-    sqrt_user_int = 
-    for i in range()
+    # Initiate sum variable.
+    sum = 0
+
+    # If user_int is divisible by a number <= its square root,
+    for i in range(1, sqrt_user_int + 1):
+        if user_int % i == 0:
+            sum += i
+
+            if i == sqrt_user_int
+            sum += user_int / i
+
 
 
 
