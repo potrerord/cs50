0746c8cc7e3f14f24764e826f1a5ab420a15965d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 02:15:06 +0000

    Mon, Jul 3, 2023, 10:15 PM -04:00

---
+++ cs7/week2/pset2/pset2d/drawing.py
@@ -0,0 +1,24 @@
+"""
+Recreates drawing specified in #16 of Pset2 Part D.
+"""
+
+
+def main():
+
+
+def top():
+    print("  _______")
+    print(" /       \\")
+    print("/         \\")
+
+
+def bottom():
+    print("\\         /")
+    print(" \\_______/")
+
+
+def chain():
+    print("-\"-'-\"-'-\"-")
+
+
+main()
