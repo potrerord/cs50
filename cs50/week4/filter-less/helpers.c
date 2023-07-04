f48351c0f1dfccddfc9e343220391985fb95f4d3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 22:15:59 +0000

    Tue, Jul 4, 2023, 6:15 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -1,23 +1,24 @@
 #include "helpers.h"
 #include <math.h>
 
-
+const int RGB_VALUES = 3;
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Iterate over all pixels in row i and column j.
-    for (int i = 0; i < height; i++)
+    // Iterate over all pixels in row px_row and column px_col.
+    for (int px_row = 0; px_row < height; px_row++)
     {
-        for (int j = 0; j < width; j++)
+        for (int px_col = 0; px_col < width; px_col++)
         {
             // Store R/G/B values for the pixel.
-            int r_orig = image[i][j].rgbtRed;
-            int g_orig = image[i][j].rgbtGreen;
-            int b_orig = image[i][j].rgbtBlue;
+            int r_orig = image[px_row][px_col].rgbtRed;
+            int g_orig = image[px_row][px_col].rgbtGreen;
+            int b_orig = image[px_row][px_col].rgbtBlue;
 
             // Take average of R/G/B values, rounded to the nearest int.
-            int gray_avg = (int) round(((r_orig + g_orig + b_orig) / 3.0));
+            int gray_avg = (int) round(((r_orig + g_orig + b_orig) /
+                                        (float) RGB_VALUES));
 
             // Update pixel values in array.
             image[i][j].rgbtRed = gray_avg;
@@ -39,25 +40,27 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int px_col = 0; px_col < width; px_col++)
         {
             // Store R/G/B values for the pixel.
-            int rgb_orig[3] = {image[px_row][px_col].rgbtRed,
-                               image[px_row][px_col].rgbtGreen,
-                               image[px_row][px_col].rgbtBlue}
+            int rgb_orig[RGB_VALUES] = {
+                image[px_row][px_col].rgbtRed,
+                image[px_row][px_col].rgbtGreen,
+                image[px_row][px_col].rgbtBlue
+            };
 
             // Store sepia conversion factors for red, green and blue.
             // Multiply each original R/G/B value by the factors in each
             // row, respectively, to get that row's sepia value.
-            float r_factors[3][3] = {
+            float r_factors[RGB_VALUES][RGB_VALUES] = {
                 {0.393, 0.769, 0.189},  // Row 0: R; Columns: R/G/B
                 {0.349, 0.686, 0.168},  // Row 1: G; Columns: R/G/B
                 {0.272, 0.534, 0.131}   // Row 2: B; Columns: R/G/B
             };
 
             // Convert R/G/B values to sepia, rounded to nearest int.
-            float rgb_sep[3] = {0.0};
+            float rgb_sep[RGB_VALUES] = {0.0};
 
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < RGB_VALUES; k++)
             {
-                for (int l = 0; l < 3; l++)
+                for (int l = 0; l < RGB_VALUES; l++)
                 {
                     rgb_sep[k] += (rgb_orig[l] * r_factors[l]);
 
@@ -77,8 +80,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                                           (b_orig * b_factors[2])));
 
             // Cap each converted sepia value at 255.
-            int rgb_sep[3] = {r_sep, g_sep, b_sep};
-            for (int k = 0; k < 3; k++)
+            int rgb_sep[RGB_VALUES] = {r_sep, g_sep, b_sep};
+            for (int k = 0; k < RGB_VALUES; k++)
             {
                 if rgb_sep[k] > 255
                 {
