695c6100ea648fdffcc5b7ca08a66c9939f6d200
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 12:49:45 +0000

    Tue, Jul 4, 2023, 8:49 AM -04:00

--- cs50/week4/volume/volume.c
+++ cs50/week4/volume/volume.c
@@ -35,7 +35,17 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
+
+
+
+
+
     // TODO: Read samples from input file and write updated data to output file
+
+
+
+
+
 
     // Close files
     fclose(input);
