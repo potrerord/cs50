ffb50de0f077d5ff30225fe2a4f28795d9acbfa1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 21:31:55 +0000

    Tue, Jul 4, 2023, 5:31 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -10,17 +10,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Store R/G/B values for the pixel.
-            int red = image[i][j].rgbtRed;
-            int green = image[i][j].rgbtGreen;
-            int blue = image[i][j].rgbtBlue;
+            int r_orig = image[i][j].rgbtRed;
+            int g_orig = image[i][j].rgbtGreen;
+            int b_orig = image[i][j].rgbtBlue;
 
             // Take average of R/G/B values, rounded to the nearest int.
-            int gray_average = (int) round(((red + green + blue) / 3.0));
+            int gray_avg = (int) round(((r_orig + g_orig + b_orig) / 3.0));
 
             // Update pixel values in array.
-            image[i][j].rgbtRed = gray_average;
-            image[i][j].rgbtGreen = gray_average;
-            image[i][j].rgbtBlue = gray_average;
+            image[i][j].rgbtRed = gray_avg;
+            image[i][j].rgbtGreen = gray_avg;
+            image[i][j].rgbtBlue = gray_avg;
         }
     }
 
@@ -37,15 +37,29 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Store R/G/B values for the pixel.
-            int red = image[i][j].rgbtRed;
-            int green = image[i][j].rgbtGreen;
-            int blue = image[i][j].rgbtBlue;
+            int r_orig = image[i][j].rgbtRed;
+            int g_orig = image[i][j].rgbtGreen;
+            int b_orig = image[i][j].rgbtBlue;
 
             // Store sepia conversion factors for red, green and blue.
             // Each factor is listed in R, G, B order.
-            float red_sepia_factors[3] = {0.393, 0.769, 0.189};
-            float green_sepia_factors[3] = {0.349, 0.686, 0.168};
-            float blue_sepia_factors[3] = {0.272, 0.534, 0.131};
+            float r_factors[3] = {0.393, 0.769, 0.189};
+            float g_factors[3] = {0.349, 0.686, 0.168};
+            float b_factors[3] = {0.272, 0.534, 0.131};
+
+
+            // Convert R/G/B values to sepia.
+            int r_sepia = int((r_orig * r_factors[0]) +
+                          (g_orig * r_factors[1]) +
+                          (b_orig * r_factors[2]))
+
+            int g_sepia = int((r_orig * g_factors[0]) +
+                          (g_orig * g_factors[1]) +
+                          (b_orig * g_factors[2]))
+
+            int b_sepia = int((r_orig * b_factors[0]) +
+                          (g_orig * b_factors[1]) +
+                          (b_orig * b_factors[2]))
 
             // Update pixel values in array.
             image[i][j].rgbtRed = ;
