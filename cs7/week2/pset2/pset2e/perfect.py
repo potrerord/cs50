6f7328c7d726c470d0a37b1e98e7e05e0c023eaf
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 03:39:39 +0000

    Mon, Jul 3, 2023, 11:39 PM -04:00

--- cs7/week2/pset2/pset2e/perfect.py
+++ cs7/week2/pset2/pset2e/perfect.py
@@ -22,12 +22,12 @@ def is_perfect_number(test_int) -> bool:
     # Special case: 1 is not a perfect number, and skipping it makes
     # later code more efficient.
     if test_int == 1:
-        return false
+        return False
 
-    # Store the square root of test_int (the square root of an integer
-    # is the upper limit for where you'll stop finding new factor
-    # pairs).
-    sqrt = test_int ** 0.5
+    # Store the int ceil of the sqrt of test_int (the square root of an
+    # integer is the upper limit for where you'll stop finding new
+    # factor pairs).
+    sqrt = int(test_int ** 0.5 + 1)
 
     # Initiate sum variable at 1 to skip first factor pair, which would
     # contain test_int, which should not be included in the sum.
@@ -35,20 +35,20 @@ def is_perfect_number(test_int) -> bool:
 
     # If user_int is divisible by a number <= its square root,
     for checked_divisor in range(2, sqrt + 1):
-        if num % checked_divisor == 0:
+        if test_int % checked_divisor == 0:
             sum += checked_divisor
 
             # Avoid adding square root twice.
             if checked_divisor < sqrt:
 
                 # Add the other member of the divisor's factor pair.
-                sum += user_int / checked_divisor
+                sum += test_int / checked_divisor
 
     # Return true if test_int is a perfect number.
     if test_int == sum:
-        return true
+        return True
 
     # Return false if not.
-    return false
+    return False
 
 main()
