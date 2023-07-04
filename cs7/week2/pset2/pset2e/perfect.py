a40790825c9898ba4af8f42183f51f21b033c318
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 03:31:30 +0000

    Mon, Jul 3, 2023, 11:31 PM -04:00

--- cs7/week2/pset2/pset2e/perfect.py
+++ cs7/week2/pset2/pset2e/perfect.py
@@ -8,22 +8,41 @@ def main():
     user_int = int(input("Enter an integer: "))
     # For every int up to and including the input
 
-    for i in range(1, user_int + 1):
+    for i in range(user_int + 1):
+        if is_perfect_number(i):
+            print(i)
 
-    # Store the square root of the int (the square root of an integer is
-    # the upper limit for where you'll stop finding new factor pairs).
-    sqrt_user_int = user_int ** 0.5
+def is_perfect_number(test_int) -> bool:
+    """Return true if test_int is a perfect number.
 
-    # Initiate sum variable.
-    sum = 0
+    An integer is perfect if it is equal to the sum of its divisors,
+    except itself.
+    """
+
+
+    # Store the square root of test_int (the square root of an integer
+    # is the upper limit for where you'll stop finding new factor
+    # pairs).
+    sqrt = test_int ** 0.5
+
+    # Initiate sum variable at 1 to skip first factor pair, which would
+    # contain test_int, which should not be included in the sum.
+    sum = 1
 
     # If user_int is divisible by a number <= its square root,
-    for i in range(1, sqrt_user_int + 1):
-        if user_int % i == 0:
-            sum += i
+    for checked_divisor in range(2, sqrt_user_int + 1):
+        if num % checked_divisor == 0:
+            sum += checked_divisor
 
-            if i == sqrt_user_int
-            sum += user_int / i
+            # Avoid adding sqrt twice, avoid adding self at all.
+            if
+
+            if checked_divisor < sqrt_user_int:
+
+                # Add the other member of the divisor's factor pair.
+                sum += user_int / checked_divisor
+
+    if
 
 
 
