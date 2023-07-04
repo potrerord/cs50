79bc11d82719da89cc3e95713aa1e3895cbabd75
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 13:45:26 +0000

    Tue, Jul 4, 2023, 9:45 AM -04:00

--- cs50/week4/volume/volume.c
+++ cs50/week4/volume/volume.c
@@ -33,14 +33,18 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Create buffer array for header.
     uint8_t header[HEADER_SIZE];
+
+    // Copy header from input to buffer to output.
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
 
-    // TODO: Read samples from input file and write updated data to output file
+    // Create buffer int to store each sample.
     int16_t buffer;
-    while(fread(&buffer, sizeof(int16_t), 1, input))
+
+    // Copy the rest of the 
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
