1858de2b20892abe6e22b760bfec45efea2b694c
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 02:29:13 +0000

    Mon, Jul 3, 2023, 10:29 PM -04:00

---
+++ cs7/week2/pset2/pset2e/triangle.py
@@ -0,0 +1,18 @@
+"""
+Prints evenly-spaced triangle of values with the following specs:
+
+- Successive rows iteratively increase in size by 1 element,
+- Successive integer elements in each row iteratively increase in value
+  at a constant rate of double the row number (0-indexed),
+- The first element of each row is a successively greater multiple of
+  100, starting with 100 and ending at 900.
+"""
+
+def main():
+    asdf
+
+
+for i 
+
+
+main()
