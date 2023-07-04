a3b512353dba140154a21217d5e20aaded34de83
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 01:19:14 +0000

    Mon, Jul 3, 2023, 9:19 PM -04:00

--- cs7/week2/pset2/pset2d/right_triangle.py
+++ cs7/week2/pset2/pset2d/right_triangle.py
@@ -3,10 +3,34 @@ Prints a right triangle with base on top and point in bottom right.
 """
 
 def main():
+    height = int(input("Enter height: "))
+    draw(height, 0)
 
 
-def print_row():
+def draw(blocks: int, spaces: int):
+    """Recursively print n rows with base case of n - 1 spaces and one
+    block.
+
+    Default "spaces" value should be 0.
+    """
+
+    # Base case: Print n - 1 spaces followed by one block.
+    if blocks <= 1:
+        for i in range(spaces):
+            print(" ", end="")
+        print("*")
+        return
+
     
+    draw(blocks - 1, spaces + 1)
 
+    # Recursive case: Print previous row with one more block and one
+    # fewer space.
+    for i in range(spaces):
+        print(" ", end="")
+    for i in range(blocks):
+        print("#", end="")
+    print()
+    return
 
 main()
