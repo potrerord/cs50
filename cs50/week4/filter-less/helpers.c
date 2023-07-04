28c542f081164288da849ebe47f0759e7c591c1e
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 20:44:00 +0000

    Tue, Jul 4, 2023, 4:44 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -3,6 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // change every pixel from BGR to be equal amounts with average intensity between the three of them
+    
+
+
+
+
+
     return;
 }
 
