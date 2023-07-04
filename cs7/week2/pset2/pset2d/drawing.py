7b86c8f2a4991fe068129fe8f61575294e38f58d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-04 02:19:49 +0000

    Mon, Jul 3, 2023, 10:19 PM -04:00

--- cs7/week2/pset2/pset2d/drawing.py
+++ cs7/week2/pset2/pset2d/drawing.py
@@ -4,6 +4,8 @@ Recreates drawing specified in #16 of Pset2 Part D.
 
 
 def main():
+    """Call other drawing functions with print() newlines."""
+    
     hexagon()
 
     print()
@@ -18,27 +20,35 @@ def main():
     bottom()
 
     print()
-    
+
     top()
     chain()
     bottom()
 
 
 def top():
+    """Draw top half of hexagon."""
+
     print("  _______")
     print(" /       \\")
     print("/         \\")
 
 
 def bottom():
+    """Draw bottom half of hexagon."""
+
     print("\\         /")
     print(" \\_______/")
 
 
 def chain():
+    """Draw chain."""
+
     print("-\"-'-\"-'-\"-")
 
 def hexagon():
+    """Draw full hexagon by combining top() and bottom()."""
+
     top()
     bottom()
 
