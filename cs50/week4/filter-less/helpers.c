ced53d975ecad4bd9277715a5c7c39eaedf17eef
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 00:01:10 +0000

    Tue, Jul 4, 2023, 8:01 PM -04:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -1,8 +1,10 @@
 #include "helpers.h"
 #include <math.h>
 
+
 // Global constant for number of RGB values.
 const int RGB_VALUES = 3;
+
 
 // Convert image to grayscale.
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -36,6 +38,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+
 // Convert image to sepia.
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -47,6 +50,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         {0.349, 0.686, 0.168},  // Row 1: GREEN; Columns: R, G, B
         {0.272, 0.534, 0.131}   // Row 2: BLUE;  Columns: R, G, B
     };
+
 
     // Create variables to store color values
     int orig[RGB_VALUES];
@@ -94,78 +98,36 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+
 // Reflect image horizontally.
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // Make buffer variable to store pixels being moved.
     int buffer;
-    int reflected_col;
+    int right_px;
 
-    // Iterate over all pixels in row px_row.
+    // Iterate over all rows.
     for (int px_row = 0; px_row < height; px_row++)
     {
 
-        // Stop iteration in each row at halfway point.
-        for (int px_col = 0, int half_width = width / 2;
-             px_col < half_width; px_col++)
+        // Stop iteration within each row at halfway point.
+        for (int left_px = 0, int half_width = width / 2;
+             left_px < half_width; px_col++)
         {
-            // Update reflected_col to
-            reflected_col = width - px_col - 1;
+            // Update the right_px value to match the left, subtracting
+            // 1 to account for zero-index.
+            right_px = width - left_px - 1;
 
             // Switch left pixel with corresponding right pixel
-
-            // stop at halfway?
-            buffer = image[px_row][px_col];
-            image[px_row][px_col] = image[px_row][reflected_col];
-            image[px_row][reflected_col] = buffer;
-
-
-            // Make a buffer variable for the entire pixel's
-
-            // original row is the same
-            // original column was px_col, now it's width - px_col - 1 bc of zero index
-
-/*
-            // Store sepia conversion factors. Multiply each original
-            // R/G/B value by the factors in each row, respectively, to
-            // get that row's sepia value.
-            float conv_factors[RGB_VALUES][RGB_VALUES] = {
-                {0.393, 0.769, 0.189},  // Row 0: R; Columns: R, G, B
-                {0.349, 0.686, 0.168},  // Row 1: G; Columns: R, G, B
-                {0.272, 0.534, 0.131}   // Row 2: B; Columns: R, G, B
-            };
-
-            // Initialize all sepia values to 0.0 for conversion sum.
-            float sepia[RGB_VALUES] = {0.0};
-
-            // Convert to sepia using conv_factors row/col structure.
-            for (int rgb_row = 0; rgb_row < RGB_VALUES; rgb_row++)
-            {
-                for (int rgb_col = 0; rgb_col < RGB_VALUES; rgb_col++)
-                {
-                    // Compute sepia values by adding respective products.
-                    sepia[rgb_row] += (conv_factors[rgb_row][rgb_col] *
-                                       orig[rgb_col]);
-                }
-
-                // Cap each converted sepia value at 255.
-                if (sepia[rgb_row] > 255)
-                {
-                    sepia[rgb_row] = 255;
-                }
-            }
-
-            // Update pixel values in array.
-            image[px_row][px_col].rgbtRed = sepia[0];
-            image[px_row][px_col].rgbtGreen = sepia[1];
-            image[px_row][px_col].rgbtBlue = sepia[2];
-
-*/
-
+            buffer = image[px_row][left_px];
+            image[px_row][left_px] = image[px_row][right_px];
+            image[px_row][right_px] = buffer;
         }
     }
+
     return;
 }
+
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
