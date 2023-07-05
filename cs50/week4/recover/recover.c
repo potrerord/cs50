27dcb76dee5805515b6894c14f317b8548ade81f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 21:53:24 +0000

    Wed, Jul 5, 2023, 5:53 PM -04:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -32,34 +32,36 @@ int main(int argc, char *argv[])
     FATBLOCK buffer;
 
     // Loop while there is still data to be read on the memory card.
-    while (fread(card, 1, FATBLOCK_SIZE, ))
-
-    // If you use malloc() you need to use free()
-
-
-    // loop for each block of 512 bytes
-    // Scan the 1-3 bytes for 0xff 0xd8 0xff, or 11111111 11011000 11111111 in binary
-    // Also scan the 4th byte to see if it starts with 0xe, or 1110 in binary
+    while (fread(card, 1, FATBLOCK_SIZE, card))
+    {
+        // If you use malloc() you need to use free()
 
 
-
-    // when you find a jpeg signature on the memory card
-        // open a new file and write to it from the memory card
-            // The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
-            // Use a loop for this
-            // printf("%03d" or i? d for demical)
+        // loop for each block of 512 bytes
+        // Scan the 1-3 bytes for 0xff 0xd8 0xff, or 11111111 11011000 11111111 in binary
+        // Also scan the 4th byte to see if it starts with 0xe, or 1110 in binary
 
 
-    // close the file when you encounter another signature
+
+        // when you find a jpeg signature on the memory card
+            // open a new file and write to it from the memory card
+                // The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
+                // Use a loop for this
+                // printf("%03d" or i? d for demical)
 
 
-    // Finish when there's no data left to read
+        // close the file when you encounter another signature
 
 
-    // Free the data you were using
+        // Finish when there's no data left to read
 
 
-    // Return 0 if successful
+        // Free the data you were using
 
+
+        // Return 0 if successful
+
+
+    }
 
 }
