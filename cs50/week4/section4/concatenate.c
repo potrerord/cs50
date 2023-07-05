a08cf25a5632de6e008217c807809ba08a157ab3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 15:59:41 +0000

    Wed, Jul 5, 2023, 11:59 AM -04:00

---
+++ cs50/week4/section4/concatenate.c
@@ -0,0 +1,19 @@
+
+#include 
+
+
+
+
+char *concatenate(char *s1, chars2)
+{
+    // TODO
+    // create the contest of this function that takes two strings and brings them together
+
+
+
+    // Discover the length of each of the strings.
+    // strlen?
+
+
+    // Allocate the memory for this new string
+}
