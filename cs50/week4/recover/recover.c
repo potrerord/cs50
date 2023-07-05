2510591e28c3a0c74fc53df8cb8fbae65bcb6565
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 19:57:53 +0000

    Wed, Jul 5, 2023, 3:57 PM -04:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Scan until you find 4 blocks of jpeg starter data
+
+
+
+    // when you find one
+
+
+
+    // open a file and write to it
 }
