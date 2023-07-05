08151650c09663df3fd057a3acbc2d708bff6e1d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 16:22:03 +0000

    Wed, Jul 5, 2023, 12:22 PM -04:00

--- cs50/week4/section4/concatenate.c
+++ cs50/week4/section4/concatenate.c
@@ -1,33 +1,57 @@
-
 #include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
+// Prototypes
+char *concatenate(char *s1, char *s2);
 
-
-
-char *concatenate(char *s1, chars2)
+int main(void)
 {
-    // TODO
-    // create the contest of this function that takes two strings and brings them together
+    // get two strings from the user
+    char *s1 = get_string("s1: ");
+    char *s2 = get_string("s2: ");
 
+    // create a third string that combines both
+    char *s3 = concatenate(s1, s2);
 
+    // print the final string
+    printf("%s\n", s3);
 
+    // free up the memory allocated to this
+    free(s3);
+}
+
+char *concatenate(char *s1, char *s2)
+{
     // Discover the length of each of the strings.
-    // strlen?
+    int length1 = strlen(s1);
+    int length2 = strlen(s2);
+    int total_length = length1 + length2;
 
+    // Allocate the memory for this new string (malloc).
+    char *result = malloc((total_length + 1) * sizeof(char));
+    if (result == NULL)
+    {
+        return result;
+    }
 
-    // Allocate the memory for this new string
+    // Copy the first string into the combo string.
+    for (int i = 0; i < length1; i++)
+    {
+        result[i] = s1[i];
+    }
 
+    // Copy the second string into the combo string.
+    for (int i = 0; i < length2; i++)
+    {
+        result[length1 + i] = s2[i];
+    }
 
-    // Copy for the first string into the combo string.
+    // Add the termination character at the end (\0).
+    result[total_length] = '\0';
 
-
-    // Same for the second string.
-
-
-    // Add the termination character at the end \0.
-
-
-    // Return the string
-
-    return result
+    // Return the string.
+    return result;
 }
