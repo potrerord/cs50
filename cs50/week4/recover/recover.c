89f2a02dc315af6794980dfe8c8a20cc8c0529dc
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 21:58:12 +0000

    Wed, Jul 5, 2023, 5:58 PM -04:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
         // Scan the 1-3 bytes for 0xff 0xd8 0xff, or 11111111 11011000 11111111 in binary
         // Also scan the 4th byte to see if it starts with 0xe, or 1110 in binary
 
-
+        
 
         // when you find a jpeg signature on the memory card
             // open a new file and write to it from the memory card
