955961b46cc6cd98d15fe5c69d070b9f3d922d45
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-05 15:31:06 +0000

    Wed, Jul 5, 2023, 11:31 AM -04:00

---
+++ cs50/week4/section4/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1;
+
+    // A pointer that holds the memory address of something, call it 'b'
+    int *b;
+
+    b = &a;
+
+    printf("The value of a is %i; ", a);
+    printf("The value of b is %i; ", b):
+}
