75462717eba4c683c0d0349766ca033c4f725008
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-06 15:01:11 +0000

    Thu, Jul 6, 2023, 11:01 AM -04:00

--- cs7/week3/pset/pset3b/longer.py
+++ cs7/week3/pset/pset3b/longer.py
@@ -1,6 +1,24 @@
-# Add your solution to the problem "longer" to this file.
+"""
+Write a function named print_longer that takes two lists as input and
+prints out the longer one as well as the last element of the list. You
+may assume the input lists are nonempty and your function should work
+with any nonempty list no matter what the size. If the two lists are the
+same length, it should print "The lists are the same length!" To give
+you practice with f-strings, when printing the longer list you may only
+use one print statement and it may only take one f-string as an
+argument. Demonstrate your function works by testing it in a main
+function. For example, print_longer(['chocolate', 'vanilla'],
+[1, 5, 9, 7]) should print
 
-def print_longer(***replace this with your code***):
-    ***replace this with your code***
+[1, 5, 9, 7] is the longer list and its last element is 7
+"""
 
-# add your run your main function here:
+def main():
+
+
+
+def print_longer():
+
+
+
+main()
