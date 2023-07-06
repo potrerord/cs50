55877b612f1c1940c660a1808525868ddcd31957
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-06 15:22:01 +0000

    Thu, Jul 6, 2023, 11:22 AM -04:00

--- cs7/week3/pset/pset3d/congress.py
+++ cs7/week3/pset/pset3d/congress.py
@@ -1,4 +1,36 @@
-# Add your solution to the problem "congress" here.
+"""
+The following requirement for membership in the Congress of the United
+States are quoted from Article I of the Constitution of the United
+States:
+
+a)  No Person shall be a Representative who shall not have attained to
+    the Age of twenty-five Years, and been seven Years a Citizen of the
+    United States, and who shall not, when elected, be an Inhabitant of
+    that State in which he shall be chosen.
+b)  No Person shall be a senator who shall not have attained to the Age
+    of thirty Years, and been nine Years a Citizen of the United States,
+    and who shall not, when elected, be an Inhabitant of that State for
+    which he shall be chosen.
+
+Write a function named eligible_for_senate that returns a boolean value,
+and another function named eligible_for_house that also returns a
+boolean value. Both functions accept two integer arguments (via
+parameters named age and length_of_citizenship), and each returns the
+corresponding congressional eligibility as a boolean value.
+
+You should show that these examples work with your functions by making
+the appropriate function calls in main:
+
+a)  Someone of age 37 and 3 years of citizenship is not eligible for
+    election to either the House or the Senate, so the functions should
+    both return False.
+    
+b)  Someone of age 47 and 8 years of citizenship is eligible for
+    election to the House but not the Senate, so
+    eligible_for_house(47, 8) should return True, but
+    eligible_for_senate(47, 8) should return False.
+"""
+
 
 # Implement eligible_for_sentate here.
 #
--- cs7/week3/pset/pset3d/horoscope.py
+++ cs7/week3/pset/pset3d/horoscope.py
@@ -1,4 +1,41 @@
-# Add your solution to the problem "horoscope" here.
+"""
+Write a Python program that prompts the user for the day and month
+(both integers) of their birthday, and then prints the appropriate
+astrological sign according to the following:
+
+Aries: Mar21-Apr19
+Taurus: Apr20-May20
+Gemini: May21-Jun20
+Cancer: Jun21-Jul22
+Leo: Jul23-Aug22
+Virgo: Aug23-Sep22
+Libra: Sep23-Oct22
+Scorpio: Oct23-Nov21
+Sagittarius: Nov22-Dec21
+Capricorn: Dec22-Jan19
+Aquarius: Jan20-Feb18
+Pisces: Feb19-Mar20
+
+Your program must implement a function
+
+def sign(month, day):
+    ...
+
+that takes the month and day as input and returns the sign. For example,
+sign(3, 21) should return the string "Aries". Sign should return the
+string "INVALID_DATE" if the date is not valid. The function sign should
+not print anything out. Feb 29 is considered a valid date. To simplify
+your sign function, you program must also implement function
+
+def before(month1, day1, month2, day2):
+    ...
+
+that takes in two dates (assumed to be valid) and returns true if
+month1, day1 is strictly before month2, day2 and returns false
+otherwise. You must make (repeated) use of the function before in sign
+to decide what the sign of the date is.
+"""
+
 
 #  Implement a comparison function for dates
 #
@@ -8,7 +45,7 @@
 def before(month1, day1, month2, day2)
     *** add your code here ***
 
-    
+
 # Implement a sign function.
 #
 # Returns the sign for the given month and day, or
