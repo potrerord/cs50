ec3dc3af116122114432994e707267b5907ea3dc
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-06 01:35:36 +0000

    Wed, Jul 5, 2023, 9:35 PM -04:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            printf("error: could not create jpeg file.\n");
+            printf("error: could not create %s\n", filename);
             return 3;
         }
         }
