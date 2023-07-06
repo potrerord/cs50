1416409973a082e18518521eb8b6767fe2394945
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-06 15:51:54 +0000

    Thu, Jul 6, 2023, 11:51 AM -04:00

--- cs7/week3/pset/pset3b/longer.py
+++ cs7/week3/pset/pset3b/longer.py
@@ -1,39 +1,49 @@
 """
 Write a function named print_longer that takes two lists as input and
-prints out the longer one as well as the last element of the list. You
-may assume the input lists are nonempty and your function should work
-with any nonempty list no matter what the size. If the two lists are the
-same length, it should print "The lists are the same length!" To give
-you practice with f-strings, when printing the longer list you may only
-use one print statement and it may only take one f-string as an
-argument. Demonstrate your function works by testing it in a main
-function. For example, print_longer(['chocolate', 'vanilla'],
-[1, 5, 9, 7]) should print
+prints out the longer one as well as the last element of the list.
 
-[1, 5, 9, 7] is the longer list and its last element is 7
+You may assume the input lists are nonempty and your function should
+work with any nonempty list no matter what the size.
+
+If the two lists are the same length, it should print
+"The lists are the same length!"
+
+To give you practice with f-strings, when printing the longer list you
+may only use one print statement and it may only take one f-string as an
+argument.
+
+Demonstrate your function works by testing it in a main
+function. For example,
+print_longer(['chocolate', 'vanilla'], [1, 5, 9, 7])
+    should print
+    [1, 5, 9, 7] is the longer list and its last element is 7
 """
 
 def main():
-    """"""
+    """Call print_longer function with preset arguments."""
+
+
     print_longer(arg1, arg2)
 
 
 def print_longer(list1, list2):
-    """Return longer list from 2 arguments. Print longer list and final
-    element of longer list.
-    """
+    """Return and print longer list from 2 arguments."""
 
+    # Check if lists are the same length as a special case.
     if len(list1) == len(list2):
         print("The lists are the same length!")
         return None
 
+    # Record longer list and print/return.
     elif len(list1) > len(list2):
         longer = list1
+
     elif len(list2) > len(list1):
         longer = list2
 
-    print(f"{} is the longer list and its last element is
+    print(f"{longer} is the longer list and its last element is
            {longer[len(longer)]}")
+
     return longer
 
 
