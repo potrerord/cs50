30ee7be22b1ea8dd2050ee4d33202b6ad2cd7db1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-06 15:26:50 +0000

    Thu, Jul 6, 2023, 11:26 AM -04:00

--- cs7/week3/pset/pset3d/change.py
+++ cs7/week3/pset/pset3d/change.py
@@ -1,9 +1,47 @@
-# Add your solution to the problem "change" here.
+"""
+Write a Python program to compute the largest absolute change that
+occurred in the price of a stock during a 10-day period. The program
+should prompt the user 10 times for integers representing the stock
+price on 10 consecutive days. If the integers input were
+
+   48  54  49  47  62  84  59  70  75  82
+
+then the program should print a message like
+
+   Largest change of 25 from  84  to  59 occurred between day #6 and day
+   #7.
+
+Your program must implement and use the following two functions
+
+   def print_change(day1_price, day2_price, day1_num):
+       ...
+
+   def find_change():
+       ...
+
+The function print_change should take the prices on the two days and the
+number of the initial day of the change and print out the message. The
+function find_change should take no parameters, prompt the user for the
+stock prices and return a list with the prices on the two days, and the
+number of the initial day of the change. Other than the user prompts,
+find_change should not print anything out. Your main function should use
+the functions find_change and print_change to execute the program.
+
+You must design your program in such a way that you do not have to save
+all the prices in memory before computing the answer. As well, modifying
+the total number of days from 10 to 30 (for example) should merely
+require updating one variable. You are not allowed to use any list,
+tuple, string variables in this problem other than the return value of
+find_change, which is a list. You are also not allowed to use 10
+variables to store the prices. You will get no credit for such a
+solution.
+"""
+
 
 # Implement a function print_change that takes in the results of your computation
 # and prints them out in a message for the user. For example:
 #
-# Largest change of 25 from  84  to  59 
+# Largest change of 25 from  84  to  59
 # occurred between day #6 and day #7.
 #
 # Must not prompt the user for anything.
--- cs7/week3/pset/pset3d/fraction_of_day.py
+++ cs7/week3/pset/pset3d/fraction_of_day.py
@@ -1,8 +1,65 @@
-# Add your solution to the problem "fraction_of_day" here.
+"""
+Given the time of day in terms of an hour, minute, second and whether it
+is AM or PM, write a Python function named fraction_of_day to calculate
+and return (not print!) the fraction of the day (a value of type float)
+that has elapsed since midnight (12:00 AM). For example,
+
+fraction_of_day(12, 0, 0, 'A')
+would evaluate to 0.0
+
+fraction_of_day(12, 0, 0, 'P')
+would evaluate to 0.5
+
+fraction_of_day(11, 59, 59, 'P')
+would evaluate to 0.999988426
+
+Hints: Convert the time into seconds since midnight. You should also
+handle the 12:00AM case specially.
+
+Write a main function that produces the following neatly formatted table
+using f- strings for formatted printing. You solution must use looping
+to print the table. You will get no credit for a solution that uses,
+say, 24 variables and/or 24 print statements instead of a loop.
+
+Time            Fraction Since Midnight
+12:00 AM        0.0000
+1:00 AM         0.0417
+2:00 AM         0.0833
+3:00 AM         0.1250
+4:00 AM         0.1667
+5:00 AM         0.2083
+6:00 AM         0.2500
+7:00 AM         0.2917
+
+CSCI-S7: Introduction to Computer Science with Python
+Problem Set 3
+Page 12
+ 8:00 AM
+ 9:00 AM
+10:00 AM
+11:00 AM
+12:00 PM
+0.3333
+0.3750
+0.4167
+0.4583
+0.5000
+0.5417
+0.5833
+0.6250
+0.6667
+0.7083
+1:00 PM
+2:00 PM
+3:00 PM
+4:00 PM
+5:00 PM
+... (Your solution should continue to 11pm)
+"""
 
 # Returns the fraction of the day elapsed since midnight.
 #
-# Must not print anything out. 
+# Must not print anything out.
 def fraction_of_day(*** replace with your parameters ***)
     *** add your code here ***
 
