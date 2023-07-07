bb7a8b3cdd2aaf7d75e61255551f3d0e003f52b0
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-07 17:06:50 +0000

    Fri, Jul 7, 2023, 1:06 PM -04:00

--- cs7/week3/pset/pset3b/print_range.py
+++ cs7/week3/pset/pset3b/print_range.py
@@ -3,33 +3,25 @@ Prints a sequential list of all integers between a defined start and end
 (inclusive).
 """
 
-"""
-Write a function named print_range that accepts two integers i, j as
-arguments and prints the sequence of numbers between the two arguments,
-prefixed with the string "[i,j]:". Print an increasing sequence if the
-first argument is smaller than the second; otherwise, print a decreasing
-sequence. If the two arguments are the same, just that number should be
-printed. Here are some sample calls to print_range:
-   print_range(2, 7)
-   print_range(19, 11)
-   print_range(5, 5)
-The output produced from these calls should be the following sequences:
-   [2,7]: 2, 3, 4, 5, 6, 7
-   [19,11]: 19, 18, 17, 16, 15, 14, 13, 12, 11
-   [5,5]: 5
-
-Your function should be tested out using a main function of your own design.
-"""
-
 
 def main():
+   """Take user input and call print_range function."""
+
+    print()
+    user_start = (int(input("Enter a starting value: "))
+    user_range = (int(input("Enter an ending value:  "))
     print_range()
+    print()
 
 
 def print_range(start: int, end: int):
-   """Prints a sequential list of all integers between a defined start and end
+   """Print a sequential list of all integers between a defined start and end
    (inclusive).
    """
+
+   if !isinstance(start, int) or !isinstance(end, int):
+      print("error: start and end must be integers")
+      return
 
    # Print opening tag.
    print(f"[{start},{end}]: ", end="")
@@ -43,21 +35,11 @@ def print_range(start: int, end: int):
    # Print list of integers.
    for i in range(start, print_end):
 
-      # Print final value with no comma.
+      # Print final value without comma and with new line.
       if i == print_end:
          print(f"{i}")
       else:
          print(f"{i}, ", end="")
-
-
-      # Make sure it prints range + 1 to get end in there
-
-      print(f"{i}, ", end="")
-   print()
-
-
-
-
 
 
 main()
