24e93362b94168e5ace6ff81c73319c8e7b66804
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-10 15:39:44 +0000

    Mon, Jul 10, 2023, 11:39 AM -04:00

--- cs50/week5/speller/dictionary.c
+++ cs50/week5/speller/dictionary.c
@@ -46,8 +46,6 @@ bool check(const char *word)
         if (i == wordlen - 1)
         {
             // Return true if the current node is a last letter.
-            
-
             if (ptr->islast == true)
             {
                 return true;
