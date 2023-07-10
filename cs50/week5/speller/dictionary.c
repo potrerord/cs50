8cebf3939d96544899adec1e158c502c4f542289
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-10 02:50:06 +0000

    Sun, Jul 9, 2023, 10:50 PM -04:00

--- cs50/week5/speller/dictionary.c
+++ cs50/week5/speller/dictionary.c
@@ -128,6 +128,58 @@ bool load(const char *dictionary)
         return false;
     }
 
+
+
+    char word[LENGTH + 1];
+
+    // Spell-check each word in text
+    char c;
+    while (fread(&c, sizeof(char), 1, file))
+    {
+        // Allow only alphabetical characters and apostrophes
+        if (isalpha(c) || (c == '\'' && index > 0))
+        {
+            // Append character to word
+            word[index] = c;
+            index++;
+
+            // Ignore alphabetical strings too long to be words
+            if (index > LENGTH)
+            {
+                // Consume remainder of alphabetical string
+                while (fread(&c, sizeof(char), 1, file) && isalpha(c));
+
+                // Prepare for new word
+                index = 0;
+            }
+        }
+
+        // Ignore words with numbers (like MS Word can)
+        else if (isdigit(c))
+        {
+            // Consume remainder of alphanumeric string
+            while (fread(&c, sizeof(char), 1, file) && isalnum(c));
+
+            // Prepare for new word
+            index = 0;
+        }
+
+        // We must have found a whole word
+        else if (index > 0)
+        {
+            // Terminate current word
+            word[index] = '\0';
+
+
+
+
+
+
+
+
+
+
+
     char word[LENGTH + 1]
 
     // Read each word in the file.
