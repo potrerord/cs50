36cd0e112cccd48a39357dd74c146ac71e1758a8
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-10 22:47:56 +0000

    Mon, Jul 10, 2023, 6:47 PM -04:00

--- cs7/week3/pset/pset3d/change.py
+++ cs7/week3/pset/pset3d/change.py
@@ -1,47 +1,7 @@
 """
-Computes the largest absolute change that occurred in the price of a
-stock during a 10-day period. The program
-should prompt the user 10 times for integers representing the stock
-price on 10 consecutive days. If the integers input were
-
-   48  54  49  47  62  84  59  70  75  82
-
-then the program should print a message like
-
-   Largest change of 25 from  84  to  59 occurred between day #6 and day
-   #7.
-
-Your program must implement and use the following two functions
-
-   def print_change(day1_price, day2_price, day1_num):
-       ...
-
-   def find_change():
-       ...
-
-The function print_change should take the prices on the two days and the
-number of the initial day of the change and print out the message. The
-function find_change should take no parameters, prompt the user for the
-stock prices and return a list with the prices on the two days, and the
-number of the initial day of the change. Other than the user prompts,
-find_change should not print anything out. Your main function should use
-the functions find_change and print_change to execute the program.
-
-You must design your program in such a way that you do not have to save
-all the prices in memory before computing the answer. As well, modifying
-the total number of days from 10 to 30 (for example) should merely
-require updating one variable. You are not allowed to use any list,
-tuple, string variables in this problem other than the return value of
-find_change, which is a list. You are also not allowed to use 10
-variables to store the prices. You will get no credit for such a
-solution.
-"""
-
-"""
 Computes and prints the largest absolute daily change in the price of a
 stock during a 10-day period.
 """
-
 
 from typing import List
 
@@ -54,14 +14,9 @@ def main():
     change_info = find_change()
     print()
 
-    # Store change_info values.
-    start_price = change_info[0]
-    end_price = change_info[1]
-    start_day = change_info[2]
-
-    # Print answer.
+    # Print largest change.
     print()
-    print_change(start_price, end_price, start_day)
+    print_change(change_info[0], change_info[1], change_info[2])
     print()
 
 
@@ -96,13 +51,12 @@ def find_change() -> List[int]:
     return change_info
 
 
-def print_change(day1_price, day2_price, day1_num):
+def print_change(day1_price: int, day2_price: int, day1_num: int) -> None:
     """Print report to terminal."""
 
     print(f"The largest change was ${abs(day2_price - day1_price)} from "
           f"${day2_price} to ${day1_price}, occurring between Day #{day1_num} "
           f"and Day #{day1_num + 1}.""")
-
 
 
 # Run the main function if script is run directly.
