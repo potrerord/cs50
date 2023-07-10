fdf1a3df6b9120538e54bc50b745794429cde491
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-10 18:36:30 +0000

    Mon, Jul 10, 2023, 2:36 PM -04:00

--- cs50/week5/speller/dictionary.c
+++ cs50/week5/speller/dictionary.c
@@ -26,12 +26,14 @@ const unsigned int N = 26;
 // Hash table
 node *table[N];
 
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
     return false;
 }
+
 
 // Hashes word to a number
 unsigned int hash(const char *word)
@@ -40,12 +42,71 @@ unsigned int hash(const char *word)
     return toupper(word[0]) - 'A';
 }
 
-// Loads dictionary into memory, returning true if successful, else false
+
+// Load dictionary into memory, return true if successful/false if not.
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // Open dictionary
+    FILE *source = fopen(dictionary, "r");
+
+    // Return false if file was not opened.
+    if (source == NULL)
+    {
+        return false;
+    }
+
+    // Create temp node for root and exit program if failed.
+    node *temp = malloc(sizeof(node));
+    if (temp == NULL)
+    {
+        printf("error: memory allocation failed\n");
+        exit(1);
+    }
+
+    // Initialize temp node.
+    initialize_node(temp);
+
+    // Assign allocated memory for node to root pointer.
+    root = temp;
+
+    // Prepare to assemble words.
+    int index = 0;
+    char word[LENGTH + 1];
+    char c;
+
+    // Assemble words from dictionary.
+    while (fread(&c, sizeof(char), 1, source))
+    {
+        // Assume all lowercase/apostrophes and separated by \n.
+        if (c != '\n')
+        {
+            // Append character to word.
+            word[index] = c;
+            index++;
+        }
+
+        // New line means the word is over.
+        else if (index > 0)
+        {
+            // Terminate current word.
+            word[index] = '\0';
+
+            // Insert word to trie.
+            insert_to_trie(root, word);
+
+            // Increment global word count.
+            word_count++;
+
+            // Prepare for next word.
+            index = 0;
+        }
+    }
+
+    // Close file and return true for successful load.
+    fclose(source);
+    return true;
 }
+
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
@@ -53,6 +114,7 @@ unsigned int size(void)
     // TODO
     return 0;
 }
+
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
