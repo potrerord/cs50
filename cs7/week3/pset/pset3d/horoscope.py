72a5b7a7b1b07d3f28c7fdf44a34496f858a7769
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-11 15:50:41 +0000

    Tue, Jul 11, 2023, 11:50 AM -04:00

--- cs7/week3/pset/pset3d/horoscope.py
+++ cs7/week3/pset/pset3d/horoscope.py
@@ -42,23 +42,28 @@ to decide what the sign of the date is.
 #  Returns true if the date (month1, day1) is
 #  earlier in the calendar than (month2, day2).
 #  Assumes the days are valid and doens't do any checking.
-def before(month1, day1, month2, day2)
-    *** add your code here ***
 
 
-# Implement a sign function.
-#
-# Returns the sign for the given month and day, or
-# "INVALID_DATE" if the date is not valid, e.g. month=10 day=54
-# Must not print anything out.
-def sign(month, day):
-    *** add your code here ***
 
-
-# Your main function should prompt the user for a month and day and
-# print out the sign using return value of the sign function.
 def main():
-    *** add your code here ***
+    """Prompt the user for a month and day, then print the return of
+    sign function.
+    """
+
+    ...
+
+
+def before(month1, day1, month2, day2):
+    """"""
+
+    ...
+
+def sign(month, day):
+    """Return the sign for the given month and day, or "INVALID_DATE" if
+    the date is not valid.
+    """
+
+    ...
 
 
 # Run the main function if script is run directly.
