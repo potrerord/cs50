4931f1b0d828592727850b3b6b17ba43c3e4bcaf
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-11 22:56:42 +0000

    Tue, Jul 11, 2023, 6:56 PM -04:00

--- cs50/week6/section/addition.py
+++ cs50/week6/section/addition.py
@@ -8,6 +8,16 @@ def main():
     if len(sys.argv) != 3:
         sys.exit("Usage: python addition.py x y")
 
+    try:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    except ValueError:
+        sys.exit("please enter two ints")
+    else:
+        xysum = x + y
+
+    print(f"{x} + {y} = {xysum}")
+
 
 # Run main function if script is run directly.
 if __name__ == "__main__":
