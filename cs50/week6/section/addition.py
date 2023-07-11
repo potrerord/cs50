db285fbe42963feca191a1b8f420b9b1f7440df4
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-11 22:51:01 +0000

    Tue, Jul 11, 2023, 6:51 PM -04:00

---
+++ cs50/week6/section/addition.py
@@ -0,0 +1,13 @@
+"""Takes two command line arguments and prints their sum."""
+
+
+import sys
+
+
+def main():
+    ...
+
+
+# Run main function if script is run directly.
+if __name__ == "__main__":
+    main()
