d22e7da9bcbddab4dbfabccba4c6d6d3ecef6217
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-11 22:40:42 +0000

    Tue, Jul 11, 2023, 6:40 PM -04:00

--- cs50/week6/section/playlist.py
+++ cs50/week6/section/playlist.py
@@ -7,6 +7,11 @@ def main():
 
     playlist = []
     # TODO: Read songs from 2018_top100.csv into playlist
+    with open("2018_top100.csv") as file:
+        file_reader = csv.DictReader(file)
+        for row in file_reader:
+            if min_tempo < tempo or tempo < max_tempo:
+                playlist.append(tempo)
 
     # TODO: Print song titles from playlist
 
