369c8965ee7a52aec3266e6d5ae628f7182f8d9a
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-11 19:00:03 +0000

    Tue, Jul 11, 2023, 3:00 PM -04:00

---
+++ cs50/week6/pset/sentimental-cash/cash.py
@@ -0,0 +1,74 @@
+"""
+Prints the minimum US coins (up to quarters) needed to make a given
+amount of change owed in cents.
+"""
+
+
+def main():
+    """Call other functions."""
+
+    # Get change owed.
+    change = get_change()
+
+    # Count number of coins.
+    count = 0
+
+    # Subtract largest possible coin and increment n by 1, starting with quarter
+    int coin = 25;
+    while (change > 0)
+    {
+        // Compare change value to highest coin value and subtract/increment if possible
+        if (change >= coin)
+        {
+            change -= coin;
+            n++;
+        }
+        else
+        {
+            // Lower coin value to next highest and loop until change is 0
+            coin = lower_coin(coin);
+        }
+    }
+
+    // Print final number of coins
+    printf("%i\n", n);
+}
+
+// Continually prompts user for positive int amount of change in cents
+int get_change(void)
+{
+    int change;
+    do
+    {
+        change = get_int("Change owed: ");
+    }
+    while (change < 0);
+    return change;
+}
+
+// Lowers coin value to next highest coin
+int lower_coin(int coin)
+{
+    int lower = 0;
+    if (coin == 25)
+    {
+        lower = 10;
+    }
+    else if (coin == 10)
+    {
+        lower = 5;
+    }
+    else if (coin == 5)
+    {
+        lower = 1;
+    }
+    else
+    {
+        printf("Error: invalid coin value\n");
+    }
+    return lower;
+
+
+# Run the main function if script is run directly.
+if __name__ == "__main__":
+    main()
