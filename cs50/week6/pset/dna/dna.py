8ee3c2c434c5439b9ed3b4103530cdf8f6ce07d5
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 01:27:35 +0000

    Tue, Jul 11, 2023, 9:27 PM -04:00

--- cs50/week6/pset/dna/dna.py
+++ cs50/week6/pset/dna/dna.py
@@ -15,7 +15,7 @@ def main():
         sys.exit("usage: python dna.py data.csv sequences.txt")
 
     # Read database file into a variable.
-    with open(sys.argv[1], "r") as database,
+    with open(sys.argv[1], "r") as database, open(sys.argv[2], "r") as sequence:
         reader = csv.DictReader(database)
         for row in reader:
 
@@ -85,4 +85,5 @@ def longest_match(sequence: str, subsequence: str) -> int:
     return longest_run
 
 
-main()
+if __name__ == "__main__":
+    main()
