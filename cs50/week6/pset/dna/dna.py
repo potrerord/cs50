3783fd167582b0a4c768b5c61bed76ca34a37615
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 01:30:58 +0000

    Tue, Jul 11, 2023, 9:30 PM -04:00

--- cs50/week6/pset/dna/dna.py
+++ cs50/week6/pset/dna/dna.py
@@ -16,8 +16,8 @@ def main():
 
     # Read database file into a variable.
     with open(sys.argv[1], "r") as database, open(sys.argv[2], "r") as sequence:
-        reader = csv.DictReader(database)
-        for row in reader:
+            reader = csv.DictReader(database)
+            for row in reader:
 
 
 
