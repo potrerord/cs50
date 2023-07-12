636833a24a883f549499c845d95c4d51f500f722
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 00:17:10 +0000

    Tue, Jul 11, 2023, 8:17 PM -04:00

--- cs50/week6/pset/sentimental-readability/readability.py
+++ cs50/week6/pset/sentimental-readability/readability.py
@@ -4,7 +4,7 @@ Prints the Coleman-Liau index reading grade level for input text.
 
 
 def main():
-    """butt"""
+    """Print grade level for input text."""
 
     # Prompt user for text to analyze
     s = input("Enter text: ")
@@ -20,27 +20,31 @@ def main():
             letters += 1
         elif char.isspace():
             words += 1
-        elif char in {'.', '!', '?'}:
+        elif char in {".", "!", "?"}:
             sentences += 1
 
     # Store rounded Coleman-Liau index
     level = round(cole_liau(letters, words, sentences))
 
     # Print Grade X
-    for grade in range()
-
-
-
+    if level < 1:
+        print("Before Grade 1.")
+    elif level > 15:
+        print("Grade 16+")
+    else:
+        for grade in range(1, 16):
+            if grade == level:
+                print("Grade {level}")
 
 
 def cole_liau(letters: int, words: int, sentences: int) -> float:
     """Return the Coleman-Liau index given the arguments."""
 
-    score = (0.0588 * letters / (100 * words)) - \
-            (0.296 * sentences / (100 * words)) - 15.8
+    score = (
+        (0.0588 * letters / (100 * words)) - (0.296 * sentences / (100 * words)) - 15.8
+    )
 
     return score
-
 
 
 if __name__ == "__main__":
