98ee8098cb1d2ab1aaa9f75e1b48f07a11d5500b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 17:17:37 +0000

    Wed, Jul 12, 2023, 1:17 PM -04:00

---
+++ cs7/week4/pset4b/craps.py
@@ -0,0 +1,47 @@
+"""Add your solution to the problem 'craps' here."""
+
+
+from random import randint
+
+
+def main():
+    """"""
+
+    ...
+
+    print("Implement me!")
+
+
+def do_roll():
+    """Return the sum of two simulated dice rolls."""
+
+    ...
+
+
+def get_point():
+    """Return a point value from a simulated craps game."""
+
+    ...
+
+
+def play_from_point(point: int) -> bool:
+    """
+    Return a boolean value indicating if the player wins the craps game,
+    continued using the value of point.
+    """
+
+    ...
+
+
+def print_roll(roll_1, roll_2) -> None:
+    """Print the results of a die roll.
+
+    Arguments:
+    roll_1, roll_2 -- integers representing the rolls.
+    """
+
+    ...
+
+
+if __name__ == "__main__":
+    main()
