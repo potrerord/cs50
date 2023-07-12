5c2778d4027dc87128a858f2b78b4c43b858f2f7
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 21:49:58 +0000

    Wed, Jul 12, 2023, 5:49 PM -04:00

---
+++ cs7/week4/-section/2.py
@@ -0,0 +1,51 @@
+
+
+
+def main():
+    ...
+
+
+"""
+a.  Write a program that asks for the user to enter in a number between
+    1-9, and if they enter a number out of that range, asks them to
+    re-enter the number (using a while loop).
+"""
+
+
+def a():
+    while True
+        try:
+            user_int = int(input("Enter a number between 1 and 9: "))
+        except ValueError:
+            print("caught exception: number must be int between 1 and 9")
+        else:
+            
+
+
+
+
+"""
+b.  Write a program that asks a user to enter a password, until their
+    answer matches the password “DouDou”
+"""
+
+
+def b():
+    ...
+
+
+"""
+c.  You've decided not to drink coffee for 10 days straight, write a
+    program (using a while loop) that asks the user to enter whether or
+    not they drank coffee each day for 30 days, and then uses a Boolean
+    value (True if you succeeded, False if you broke and drank coffee
+    within the range) to state whether or not the user was successful.
+"""
+
+
+def c():
+    ...
+
+
+if __name__ == "__main__":
+    main()
