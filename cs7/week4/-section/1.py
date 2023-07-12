52a47c5dd5dfa8c635b4123b17c337db7fd14317
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 21:24:16 +0000

    Wed, Jul 12, 2023, 5:24 PM -04:00

---
+++ cs7/week4/-section/1.py
@@ -0,0 +1,26 @@
+# 1a.
+for i in range(5):
+    print(i)
+
+# 1b.
+for i in range(0, 30, 3):
+    print(i)
+
+# 1c.
+for i in range(30, 6, -3):
+    print(i)
+
+# 1d.
+"""Write a program that randomly generates numbers in the range [1,10]
+and prints them, until it generates a 6, at which point it prints it and
+stops."""
+
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
