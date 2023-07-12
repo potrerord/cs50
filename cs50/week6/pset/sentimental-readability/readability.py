b5a5ba42e37c59a72323c9f82beb14c2de9c8215
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-12 00:21:08 +0000

    Tue, Jul 11, 2023, 8:21 PM -04:00

--- cs50/week6/pset/sentimental-readability/readability.py
+++ cs50/week6/pset/sentimental-readability/readability.py
@@ -41,7 +41,7 @@ def cole_liau(letters: int, words: int, sentences: int) -> float:
     """Return the Coleman-Liau index given the arguments."""
 
     score = (
-        (0.0588 * (100 * (letters / words)) - (0.296 * (100 sentences / words)) - 15.8
+        (0.0588 * (100 * (letters / words)) - (0.296 * (100 * sentences / words)) - 15.8
     )
 
     return score
