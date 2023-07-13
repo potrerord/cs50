29a163a75ecdde9111c36d5a483a8f61de76cc1b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-13 22:54:11 +0000

    Thu, Jul 13, 2023, 6:54 PM -04:00

---
+++ cs50/week7/lecture/favorites.py
@@ -0,0 +1,22 @@
+"""
+Following along with Week 7.
+
+favorites.csv data:
+timestamp,language,problem
+"""
+
+
+import csv
+
+
+def main():
+    """Mess with the data of favorite CS50 problems."""
+
+    with open("favorites.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row[i])
+
+
+if __name__ == "__main__":
+    main()
