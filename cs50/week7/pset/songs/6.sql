c82dceeeab6e6f498d30ce7083f8ef928bb3e3be
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-14 20:52:35 +0000

    Fri, Jul 14, 2023, 4:52 PM -04:00

--- cs50/week7/pset/songs/6.sql
+++ cs50/week7/pset/songs/6.sql
@@ -1,5 +1,6 @@
 /* The names of songs that are by Post Malone. */
-SELECT name
+SELECT s.name
   FROM songs AS s
        JOIN artists AS a
        ON s.artist_id = a.id
+          AND a.name = 'Post Malone';
