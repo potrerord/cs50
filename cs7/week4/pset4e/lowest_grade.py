49994293a2fd8a9542310fe479fe5dc55bb93024
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 22:11:17 +0000

    Sat, Jul 15, 2023, 6:11 PM -04:00

--- cs7/week4/pset4e/lowest_grade.py
+++ cs7/week4/pset4e/lowest_grade.py
@@ -1,38 +1,5 @@
 """
-It is common in some courses (though not in CSCI-S7) for an instructor
-to not count the lowest exam score or homework score for each student.
-
-Define a function named remove_lowest that accepts a list of numbers
-that represent the homework scores of a single student.
-
-Your function should return a list that contains all of the values
-passed to the function EXCEPT for the lowest score. Your function should
-not modify the original list. To illustrate how this function ought to
-work, consider the following code:
-
-    x = [23, 90, 47, 55, 88]
-    a = remove_lowest(x)
-    b = remove_lowest([85])
-    c = remove_lowest([])
-    d = remove_lowest([59, 92, 93, 47, 88, 47])
-    print("x =", x)
-    print("a =", a)
-    print("b =", b)
-    print("c =", c)
-    print("d =", d)
-
-The correct output would be
-
-    x = [23, 90, 47, 55, 88]
-    a = [90, 47, 55, 88]
-    b = [85]
-    c = []
-    d = [59, 92, 93, 88, 47]
-
-Note that in the case of a, the lowest score, 23, was removed.
-
-The fourth case, d, shows what happens when the lowest score appears
-twice: it is removed only once from the list that is constructed.
+Removes the lowest score from a list of scores.
 """
 
 
@@ -78,8 +45,6 @@ def remove_lowest(grade_list: List[int]) -> List[int]:
     new_list.remove(lowest_grade)
 
     return new_list
-
-
 
 
 if __name__ == "__main__":
--- cs7/week4/pset4e/roman.py
+++ cs7/week4/pset4e/roman.py
@@ -1,4 +1,53 @@
-"""Add your solution to the problem 'roman' here."""
+"""
+Write a program to read in a string of characters that represent a Roman
+numeral and then convert it to Arabic form (an integer). The character
+values for Roman numerals are as follows:
+
+Symbol  Value
+M       1000
+D       500
+C       100
+L       50
+X       10
+V       5
+I       1
+
+You may assume that the user inputs a valid Roman numeral representing a
+value between 1 and 3999. Test your program with the following data:
+
+LXXXVII (87)
+CCXIX (219)
+MCCCLIV (1354)
+MMDCLXXIII (2673)
+MCDLXXVI (1476)
+
+For your reference, the following link has a chart of many Roman
+numerals: http://literacy.kent.edu/Minigrants/Cinci/romanchart.htm
+
+You must define a function
+
+def to_arabic(roman_numeral):
+    ...
+
+that takes a valid Roman numeral and converts it to the corresponding
+decimal number.
+
+5 points bonus Extra Credit
+Extend your Roman numerals solutions to print an error if an invalid
+Roman numeral is input. A numeral could be invalid because it includes
+invalid characters, such as A, B, or is lowercase. In addition, numerals
+like IXI, VC, and XVIIIII that have too many symbols or symbols in the
+wrong order are also invalid. The form XXXXVIIII is also considered
+invalid for this problem. Your program should reject all invalid Roman
+numerals, including ones with the wrong order or too many symbols.
+
+You must define a function
+def is valid(roman_numeral):
+    ...
+
+that takes a Roman numeral and returns True if it is valid and False if
+it is not.
+"""
 
 
 def main():
