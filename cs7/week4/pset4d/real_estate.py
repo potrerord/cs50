d89ab823595bb6c6aaff60fc944cca10bf0aca8f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 20:46:54 +0000

    Sat, Jul 15, 2023, 4:46 PM -04:00

--- cs7/week4/pset4d/real_estate.py
+++ cs7/week4/pset4d/real_estate.py
@@ -1,24 +1,7 @@
-"""Real estate agent advertisements frequently contain words from which
-all vowels — except for an initial one — have been removed. Thus,
-
-     Desirable unfurnished flat in quiet residential area
-
-might become
-
-    Dsrbl unfrnshd flt in qt rsdntl ar
-
-Write a complete Python program that reads in a normal one-line
-description of a property and prints the corresponding advertisement.
-Your program must implement a function
-
-def devowel(ad):
-    ...
-which takes the string description and returns the corresponding
-vowelless ad as a string. It must also implement and use a function
-
-def is_vowel(c): ...
-that returns True if a character is vowel, a, e, i, o, u, A, E, I, O, U.
-You may assume that y is not a vowel."""
+"""
+Reads a normal one-line description of a property and prints a "real-
+estate-style" advertisement with all non-inital vowels removed.
+"""
 
 
 def main():
