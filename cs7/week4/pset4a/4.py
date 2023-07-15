e15891e3eb400cf4b25c0efd4c01f2344b1e68b4
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 17:57:44 +0000

    Sat, Jul 15, 2023, 1:57 PM -04:00

--- cs7/week4/pset4a/4.py
+++ cs7/week4/pset4a/4.py
@@ -1,23 +1,47 @@
-"""1"""
+# """1.1"""
 
-y = ['a', 'b', 'c']
-y[2] = 'd'
-print(y)
+# y = ['a', 'b', 'c']
+# y[2] = 'd'
+# print(y)
 
-"""2"""
+# """1.2"""
 
-y = ['a', 'b', 'c']
-y.append('d')
-print(y)
+# y = ['a', 'b', 'c']
+# y.append('d')
+# print(y)
 
 
-"""3"""
+# """1.3"""
 
-y = ['a', 'b', 'a']
-y.remove('a')
-print(y)
+# y = ['a', 'b', 'a']
+# y.remove('a')
+# print(y)
 
-"""4"""
+# """1.4"""
+
+# y = ['a', 'b', 'c']
+# y.insert(2, 'a')
+# print(y)
+#
+
+"""2.1"""
+foobar = [1, 4, 9, 16, 25]
+for element in foobar:
+    element = 5
+print(foobar)
+
+
+"""2.2"""
+
+
+
+
+"""2.3"""
+
+
+
+
+"""2.4"""
 
 
 
