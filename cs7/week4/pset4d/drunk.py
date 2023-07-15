fc2eff6d5c591f46fa814f906b41b32cf70d5ef6
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 20:48:53 +0000

    Sat, Jul 15, 2023, 4:48 PM -04:00

--- cs7/week4/pset4d/drunk.py
+++ cs7/week4/pset4d/drunk.py
@@ -1,15 +1,60 @@
-"""Add your solution to the problem 'drunk' here."""
+"""
+A Harvard student has decided to celebrate just graduating by getting a
+bit smashed.
+
+Their home is at 1st Street and Main Street, and the jail is at 11th
+Street and Main Street. The student starts at 6th Street and Main Street
+and randomly chooses to wander one block up or down Main Street with
+equal probability, 0.5. At each intersection they repeat the process
+until arriving safely at home or landing in jail. That is, at each
+intersection, the drunk student has a 50-50 probability of staggering a
+block one way or the other, to the next higher-numbered or next lower-
+numbered street.
+
+
+Write a function named drunk_walk() using a while loop that simulates
+the drunk student's walk; your function should return an list or a tuple
+with an integer that indicates how many blocks were walked as well as a
+Boolean that indicates whether they ended up at home or in jail. The
+function drunk_walk should not print out anything. Your program should
+print out how many blocks were walked and where the student landed. You
+should not print out each step taken in your final version of the
+function, though you might want to do this while you are debugging.
+
+Once you have your function working, have your main program call upon
+your drunk_walk() function N times. Finally, have it calculate and print
+the average number of blocks that the student took for one trip, to one
+decimal place. Here's what your program might look like in action, with
+N equal to 5:
+       Here we go again... time for a walk!
+       Walked 37 blocks, and
+       Landed at HOME
+       Here we go again... time for a walk!
+       Walked 19 blocks, and
+       Landed in JAIL
+       Here we go again... time for a walk!
+       Walked 13 blocks, and
+       Landed in JAIL
+       Here we go again... time for a walk!
+       Walked 25 blocks, and
+       Landed in JAIL
+       Here we go again... time for a walk!
+       Walked 15 blocks, and
+       Landed at HOME
+       Average # of blocks equals 21.8
+"""
 
 
 def main():
-    # Add your solution to the problem that makes use of the above.
-    print("Implement me!")
+    """Add your solution to the problem that makes use of the above."""
 
 
 def drunk_walk():
-    """Simulates a random walk. Returns the number blocks walked as well as
-    a boolean that is True if ended up at home and False ended up in jail.
-    Takes no input parameters. Must not print anything!
+    """Simulate a random walk. Return the number blocks walked as well
+    as a boolean that is True if ended up at home and False ended up in
+    jail.
+
+    Take no input parameters. Do not print anything.
     """
     # Add your code here
 
