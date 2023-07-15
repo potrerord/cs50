ddc5981d9845242905ecd698786015fa8acbc0a5
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 17:10:00 +0000

    Sat, Jul 15, 2023, 1:10 PM -04:00

---
+++ cs7/week4/pset4a/4a.py
@@ -0,0 +1,21 @@
+"""
+1.  s.count("o")
+2.  s.count('stuck')
+3.  s.find('Friend')
+4.  s.find('enemy', 4)
+5.  s[2].islower()
+6.  s[0:5] == 'Your'
+7.  s[0:5].upper()
+"""
+
+
+def main():
+    """Test the questions."""
+
+    s = "Your Friend Is Stuck. You Give Me Money, I Make Him Unstuck."
+
+    question = s.find('enemy', 4)
+    print(question)
+
+if __name__ == "__main__":
+    main()
