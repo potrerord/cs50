1eb97e84acb3caff74f1427c4c129bbdf062508b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 16:13:46 +0000

    Sat, Jul 15, 2023, 12:13 PM -04:00

--- cs50/week7/section/enroll.py
+++ cs50/week7/section/enroll.py
@@ -13,11 +13,12 @@ Usage:
 
 
 from cs50 import get_string, SQL
+import sys
 
 
 def main():
 
-    database = SQL("sqlite:///test.db")
+    database = SQL("sqlite:///students.db")
 
     #add a person
     name = get_string("Name: ")
@@ -29,14 +30,24 @@ def main():
         code = get_string("Course code: ")
 
         #if no input, then stop adding - TODO
+        if code == "":
+            sys.exit(1)
 
         #query for course - TODO
+        count = db.execute("SELECT COUNT * FROM ")
+
+
 
         #check to make sure course exists - TODO
 
+
+
+
         #enroll student - TODO
 
-        ...
+
+
+
 
 
 if __name__ == "__main__":
