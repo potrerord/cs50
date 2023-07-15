2448d897b0376259d4377eb13e03b87eb394d1b8
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 19:05:51 +0000

    Sat, Jul 15, 2023, 3:05 PM -04:00

--- cs7/week4/pset4b/craps.py
+++ cs7/week4/pset4b/craps.py
@@ -1,46 +1,12 @@
 """
-Write a complete Python program that plays a simplified version of
-the casino game Craps. This game is played with a set of two perfectly
-balanced dice, each one a cube that has one side containing 1, 2, 3, 4,
-5, or 6 black dots.
-
-Your program will “roll” two dice until the total value is 4, 5, 6, 8,
-9, or 10. This number becomes the player's point.
-
-After the point value has been obtained, the program continues to roll
-the dice until the total value of the dice is either:
-    - 7 (in which case you lose), or
-    - it's the point that was previously established (in which case you
-      win).
-
-Please use the random.randint(...) function to generate your rolls.
-This problem is an exercise in implementing a solution according to the
-interface of functions we have given you. Here are the functions you
-need to define and use in your solution.
-
-def do_roll():
-   do_roll takes no input, rolls two dice and returns the sum of the
-   roll.
-
-def print_roll(roll_1, roll_2):
-    print_roll takes the values of the two dice rolls as input and
-    prints out the results to the user as shown below. It does not
-    return any value.
-
-def get_point():
-    get_point takes no input, rolls until a point is established and
-    returns the value of the point.
-
-def play_from_point(point):
-    play_from_point takes an integer point value and continues the game
-    until the player wins or loses. It returns a Boolean value which is
-    True if the player wins.
-
+Plays a simplified version of the casino game Craps between the user and
+the program.
+"""
+"""
 Note: it is recommended that there are two separate while loops in your
 implementation of this game. Here's what the program should look like in
 action:
-
-    % python craps.py
+    $ python craps.py
     Computer rolls a 6 and a 5, for a total of 11.
     Computer rolls a 1 and a 2, for a total of 3.
     Computer rolls a 5 and a 1, for a total of 6.
@@ -55,47 +21,97 @@ from random import randint
 
 
 def main():
-    """"""
+    """Execute game by rolling dice and comparing points."""
 
-    ...
+    # Roll two dice until the total value is 4-10, which becomes the
+    # player's point.
 
-    print("Implement me!")
+    while True:
+        player_point = do_roll()
+        if 4 <= player_point <= 10:
+            break
+
+    # Continue to roll the dice.
+
+
+
+
+    # If total value of the dice is 7,
+    # You lose.
+
+
+
+
+    # If total value of the dice is the point that was previously
+    # established,
+    # You win.
+
+
+
+    # Computer rolls a 6 and a 5, for a total of 11.
+    # Computer rolls a 1 and a 2, for a total of 3.
+    # Computer rolls a 5 and a 1, for a total of 6.
+    while True:
+        print_roll(arg1, arg2)
+        if point == 6:
+            break
+
+    # 6 is now the established POINT.
+
+
+    # Computer rolls a 4 and a 4, for a total of 8.
+    # Computer rolls a 2 and a 5, for a total of 7.
+    # YOU LOSE
+
 
 
 def do_roll() -> int:
-    """Return the sum of two simulated dice rolls."""
+    """Take no input, roll two dice and return the sum of the roll."""
 
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
+    die1 = randint(1, 6)
+    die2 = randint(1, 6)
     dice_sum = die1 + die2
 
     return dice_sum
 
 
 def get_point():
-    """Return a point value from a simulated craps game."""
+    """Roll until a point is established, then return point value."""
 
-    ...
+    while True:
+        do_roll()
 
 
 def play_from_point(point: int) -> bool:
     """
-    Return a boolean value indicating if the player wins the craps game,
-    continued using the value of point.
-    """
-
-    ...
-
-
-def print_roll(roll_1, roll_2) -> None:
-    """Print the results of a die roll.
+    Continue the craps game until a winner is decided. Return a Boolean
+    value indicating if the player won.
 
     Arguments:
-    roll_1, roll_2 -- integers representing the rolls.
+    point -- Integer value of computer's point for player to play from.
     """
 
-    ...
+    # Continue the game from the point.
 
+
+    # If the player won,
+    return True
+
+    # If the player lost,
+    return False
+
+
+
+def print_roll(roll1, roll2) -> None:
+    """Take the values of two dice rolls as input and print results for
+    the user. Do not return any value.
+
+    Arguments:
+    roll1, roll2 -- integers representing the rolls.
+    """
+
+    print(f"Computer rolls a {roll1} and a {roll2}, \
+          for a total of {roll1 + roll2}.")
 
 if __name__ == "__main__":
     main()
