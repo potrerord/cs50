d8f4c0cd38a585f6f5b5a41bdd3ce018ec27ac32
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-15 18:40:09 +0000

    Sat, Jul 15, 2023, 2:40 PM -04:00

--- cs7/week4/pset4a/4.py
+++ cs7/week4/pset4a/4.py
@@ -1,46 +1,43 @@
-# """1.1"""
-
-# y = ['a', 'b', 'c']
-# y[2] = 'd'
-# print(y)
-
-# """1.2"""
-
-# y = ['a', 'b', 'c']
-# y.append('d')
-# print(y)
+# 1.1
+y = ['a', 'b', 'c']
+y[2] = 'd'
+print(y)
 
 
-# """1.3"""
+# 1.2
+y = ['a', 'b', 'c']
+y.append('d')
+print(y)
 
-# y = ['a', 'b', 'a']
-# y.remove('a')
-# print(y)
 
-# """1.4"""
+# 1.3
+y = ['a', 'b', 'a']
+y.remove('a')
+print(y)
 
-# y = ['a', 'b', 'c']
-# y.insert(2, 'a')
-# print(y)
-#
 
-"""2.1"""
+# 1.4
+y = ['a', 'b', 'c']
+y.insert(2, 'a')
+print(y)
+
+
+# 2.1
+foobar = [-1, 4, -9, 16, -25]
+foobar = [5 for _ in foobar]
+print(foobar)
+
+# 2.2
+foobar = [-1, 4, -9, 16, -25]
+foobar = foobar[-1:] + foobar[:-1]
+print(foobar)
+
+# 2.3
+foobar = [-1, 4, -9, 16, -25]
+foobar[0], foobar[-1] = foobar[-1], foobar[0]
+print(foobar)
+
+# 2.4
 foobar = [-1, 4, -9, 16, -25]
 foobar = [abs(element) for element in foobar]
 print(foobar)
-
-
-"""2.2"""
-
-
-
-
-"""2.3"""
-
-
-
-
-"""2.4"""
-
-
-
