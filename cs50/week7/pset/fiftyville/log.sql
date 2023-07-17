2443de19bce23927be8986efc9960107aab37959
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 22:04:36 +0000

    Mon, Jul 17, 2023, 6:04 PM -04:00

--- cs50/week7/pset/fiftyville/log.sql
+++ cs50/week7/pset/fiftyville/log.sql
@@ -19,7 +19,52 @@ took place on Humphrey Street.
 -- Starting with a .sch to get a feel for the general layout.
 .sch
 
--- 
+/*
+10 Tables:
+
+1)  crime_scene_reports
+    -   descriptions will be worth looking at
+    -   not sure what the id primary key means in this context -
+            crime id?
+
+2)  interviews
+    -   this has transcripts in it, probably should look at all of
+        those
+
+3)  atm_transactions
+    -   will likely be relevant for the escape? paying for flight?
+
+4)  bank_accounts
+    -   if they're dumb enough to use their bank account for this then
+        maybe it's related to escape/flight
+
+5)  airports
+    -   maybe will use this to figure out where they landed? idk if this
+        will be useful if i have flight info
+
+6)  FLIGHTS
+    -   WILL CONTAIN ITEM #2: CITY
+
+7)  passengers
+    -   probably will use this to verify suspect is on plane to
+        destination city
+    -   not sure if this is like just a list of sold tickets or like a
+        confirmed list of passengers in attendance on the plane
+
+8)  phone_calls
+    -   no transcripts or anything here
+
+9)  PEOPLE
+    -   WILL CONTAIN ITEM #1: NAME OF THIEF
+    -   WILL CONTAIN ITEM #3: NAME OF ACCOMPLICE
+    -   this has the primary key id thing too so maybe it connects to
+        the other tables? seems crazy to associate a crime id with a
+        person
+
+10) bakery_security_logs
+    -
+
+*/
 
 
 
