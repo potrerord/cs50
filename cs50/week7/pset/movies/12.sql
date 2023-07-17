f8392dd30469153d6de631d3a93a014fca4bf8d3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 20:21:41 +0000

    Mon, Jul 17, 2023, 4:21 PM -04:00

--- cs50/week7/pset/movies/12.sql
+++ cs50/week7/pset/movies/12.sql
@@ -2,13 +2,14 @@
 List the titles of all movies in which both Bradley Cooper and Jennifer
 Lawrence starred.
 */
-SELECT DISTINCT(title)
+SELECT DISTINCT(title)  -- no duplicates
   FROM movies AS m
        INNER JOIN stars AS s
        ON m.id = s.movie_id
 
        INNER JOIN people AS p
        ON s.person_id = p.id
+-- All movies starring either Bradley Cooper or Jennifer Lawrence
  WHERE p.name IN ('Bradley Cooper', 'Jennifer Lawrence')
- GROUP BY m.id;
- 
+ GROUP BY m.id  -- group the same movie
+HAVING COUNT(m.id) > 1;  -- select the groups with both actors
