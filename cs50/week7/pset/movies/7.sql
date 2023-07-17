cfbb5291ab484c96e535fa44676491b9ebe30026
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 19:01:37 +0000

    Mon, Jul 17, 2023, 3:01 PM -04:00

--- cs50/week7/pset/movies/7.sql
+++ cs50/week7/pset/movies/7.sql
@@ -4,7 +4,7 @@ by rating. For movies with the same rating, order them alphabetically by
 title.
 */
 
-SELECT title
+SELECT title, rating
   FROM movies AS m
        JOIN ratings AS r
        ON m.id = r.movie_id
