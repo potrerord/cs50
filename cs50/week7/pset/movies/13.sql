f8d796f209c205fc9c10aefff81f4dd83803739f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 20:28:08 +0000

    Mon, Jul 17, 2023, 4:28 PM -04:00

--- cs50/week7/pset/movies/13.sql
+++ cs50/week7/pset/movies/13.sql
@@ -3,7 +3,7 @@ List the names of all people who starred in a movie in which Kevin Bacon
 also starred.
 */
 
-SELECT name
+SELECT p.name
   FROM people AS p
        JOIN stars AS s
        ON p.id = s.person_id
