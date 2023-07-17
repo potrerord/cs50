d7a79291329bccfc45ef77bc1b9e8fb6f42583fe
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 17:30:55 +0000

    Mon, Jul 17, 2023, 1:30 PM -04:00

--- cs50/week7/pset/movies/6.sql
+++ cs50/week7/pset/movies/6.sql
@@ -1,2 +1,7 @@
 /* Determine the average rating of all movies released in 2012. */
 
+SELECT AVG(rating)
+  FROM ratings AS r
+       JOIN movies AS m
+       ON r.movie_id = m.id
+ WHERE m.year = 2012;
--- cs50/week7/pset/movies/7.sql
+++ cs50/week7/pset/movies/7.sql
@@ -4,3 +4,9 @@ by rating. For movies with the same rating, order them alphabetically by
 title.
 */
 
+SELECT title
+  FROM movies AS m
+       JOIN ratings AS r
+       ON m.id = r.movie_id
+ WHERE m.year = 2010
+ ORDER BY rating, title;
