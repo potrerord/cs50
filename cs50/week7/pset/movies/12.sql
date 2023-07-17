d0e24d05145423ba4078d601b2f882d910521469
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 18:44:41 +0000

    Mon, Jul 17, 2023, 2:44 PM -04:00

--- cs50/week7/pset/movies/12.sql
+++ cs50/week7/pset/movies/12.sql
@@ -3,44 +3,30 @@ List the titles of all movies in which both Bradley Cooper and Jennifer
 Lawrence starred.
 */
 
-CREATE VIEW view_name AS
-SELECT column1, column2
-FROM table1
-JOIN table2
-ON table1.common_column = table2.common_column;
+CREATE VIEW jlaw_bcoop_movies AS
+    SELECT *
+      FROM movies AS m
+           JOIN stars AS s
+           ON m.id = s.movie_id
 
-
+           JOIN people AS p
+           ON s.person_id = p.id
+              AND (p.name = 'Bradley Cooper'
+                   OR p.name = 'Jennifer Lawrence')
 
 SELECT DISTINCT(movies.title)
-  FROM movies AS m
-       JOIN stars AS s
-       ON m.id = s.movie_id
-
-       JOIN people AS p
-       ON s.person_id = p.id
-          AND (p.name = 'Bradley Cooper'
-               OR p.name = 'Jennifer Lawrence')
+  FROM jlaw_bcoop_movies AS jbmovies
 
  -- Movie matches with both of the following:
  WHERE m.id IN
        -- Movies starring Bradley Cooper
        (SELECT m.id
-         FROM movies AS m
-              JOIN stars AS s
-              ON m.id = s.movie_id
-
-              JOIN people AS p
-              ON s.person_id = p.id
-        WHERE p.name = 'Bradley Cooper'
+          FROM jbmovies
+         WHERE p.name = 'Bradley Cooper'
        )
    AND m.id IN
        -- Movies starring Jennifer Lawrence
        (SELECT m.id
-         FROM movies AS m
-              JOIN stars AS s
-              ON m.id = s.movie_id
-
-              JOIN people AS p
-              ON s.person_id = p.id
-        WHERE p.name = 'Jennifer Lawrence'
+          FROM jbmovies
+         WHERE p.name = 'Jennifer Lawrence'
        );
