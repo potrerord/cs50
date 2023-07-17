92d2948d95832861248f5c124ba53384c2742f8d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 19:03:18 +0000

    Mon, Jul 17, 2023, 3:03 PM -04:00

--- cs50/week7/pset/movies/13.sql
+++ cs50/week7/pset/movies/13.sql
@@ -20,6 +20,5 @@ SELECT name
                JOIN people AS p
                ON s.person_id = p.id
          WHERE p.name = 'Kevin Bacon'
-           AND p.birth = 1958
        )
    AND p.name != 'Kevin Bacon';
