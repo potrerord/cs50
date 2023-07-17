bbc0bdb0018f10277d4a0bb2e956199b08b5736f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 18:23:13 +0000

    Mon, Jul 17, 2023, 2:23 PM -04:00

--- cs50/week7/pset/movies/12.sql
+++ cs50/week7/pset/movies/12.sql
@@ -3,7 +3,7 @@ List the titles of all movies in which both Bradley Cooper and Jennifer
 Lawrence starred.
 */
 
-SELECT title
+SELECT DISTINCT(title)
   FROM movies AS m
        JOIN stars AS s
        ON m.id = s.movie_id
@@ -12,24 +12,23 @@ SELECT title
        ON s.person_id = p.id
           AND (p.name = 'Bradley Cooper'
                OR p.name = 'Jennifer Lawrence')
- WHERE m.id =
-       (SELECT id
-          FROM people AS p
-         WHERE p.name = 'Bradley Cooper'
+ WHERE m.id IN
+       (SELECT m.id
+         FROM movies AS m
+              JOIN stars AS s
+              ON m.id = s.movie_id
+
+              JOIN people AS p
+              ON s.person_id = p.id
+              WHERE p.name = 'Bradley Cooper'
        )
-   AND m.id =
-       (SELECT id
-          FROM people AS p
-         WHERE p.name = 'Jennifer Lawrence'
+   AND m.id IN
+       (SELECT m.id
+         FROM movies AS m
+              JOIN stars AS s
+              ON m.id = s.movie_id
+
+              JOIN people AS p
+              ON s.person_id = p.id
+              WHERE p.name = 'Jennifer Lawrence'
        );
-
-
--- List of movies where bcoop starred
-SELECT id
-  FROM movies AS m
-       JOIN stars AS s
-       ON m.id = s.movie_id
-
-       JOIN people AS p
-       ON s.person_id = p.id
- WHERE s.person_id 
