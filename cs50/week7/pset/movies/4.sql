63e8efa8afbc44a8a1b7b9da63d24af86c667328
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 19:10:56 +0000

    Mon, Jul 17, 2023, 3:10 PM -04:00

--- cs50/week7/pset/movies/4.sql
+++ cs50/week7/pset/movies/4.sql
@@ -1,8 +1,6 @@
 /* Determine the number of movies with an IMDb rating of 10.0. */
-
 SELECT COUNT(*)
   FROM movies AS m
        JOIN ratings AS r
        ON m.id = r.movie_id
  WHERE r.rating = 10.0;
- 
