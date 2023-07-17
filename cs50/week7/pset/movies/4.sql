57dc2cf8417d3d6b7e10eee35d34c87c640e9e73
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 17:26:47 +0000

    Mon, Jul 17, 2023, 1:26 PM -04:00

--- cs50/week7/pset/movies/4.sql
+++ cs50/week7/pset/movies/4.sql
@@ -1,7 +1,8 @@
 /* Determine the number of movies with an IMDb rating of 10.0. */
 
 SELECT COUNT(*)
-  FROM movies
-       JOIN ratings
-       ON 
- WHERE
+  FROM movies AS m
+       JOIN ratings AS r
+       ON m.id = r.movie_id
+ WHERE r.rating = 10.0;
+ 
--- cs50/week7/pset/movies/5.sql
+++ cs50/week7/pset/movies/5.sql
@@ -3,3 +3,7 @@ List the titles and release years of all Harry Potter movies, in
 chronological order.
 */
 
+SELECT title,
+       year
+  FROM movies
+ WHERE title LIKE 
