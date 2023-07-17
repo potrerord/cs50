85fac33a577c08dd638db0d0c62fb1a297a5ac6d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 21:51:42 +0000

    Mon, Jul 17, 2023, 5:51 PM -04:00

--- cs50/week7/pset/fiftyville/log.sql
+++ cs50/week7/pset/fiftyville/log.sql
@@ -1,5 +1,36 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+/*
+Note to self: All you know is that the theft took place on July 28, 2021
+and that it took place on Humphrey Street.
+*/
+
 -- Starting with a .sch to get a feel for the general layout.
 .sch
+
+-- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
