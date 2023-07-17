1981c2d89e290ed9003a5f452784a37354399649
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 21:53:18 +0000

    Mon, Jul 17, 2023, 5:53 PM -04:00

--- cs50/week7/pset/fiftyville/log.sql
+++ cs50/week7/pset/fiftyville/log.sql
@@ -1,8 +1,19 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 /*
-Note to self: All you know is that the theft took place on July 28, 2021
-and that it took place on Humphrey Street.
+Note to self:
+
+Authorities believe that the thief stole the duck and then, shortly
+afterwards, took a flight out of town with the help of an accomplice.
+Your goal is to identify:
+
+Who the thief is,
+What city the thief escaped to, and
+Who the thief’s accomplice is who helped them escape
+
+
+All you know is that the theft took place on July 28, 2021 and that it
+took place on Humphrey Street.
 */
 
 -- Starting with a .sch to get a feel for the general layout.
