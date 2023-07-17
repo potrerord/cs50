3f8899763cba7280fd0d8c57d6e37449dfc94edd
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-17 21:22:09 +0000

    Mon, Jul 17, 2023, 5:22 PM -04:00

--- cs50/week7/pset/movies/13.sql
+++ cs50/week7/pset/movies/13.sql
@@ -7,18 +7,15 @@ also starred.
 -- Names of stars of movies starring Kevin Bacon
 SELECT p1.name
   FROM people AS p1
- WHERE p1.id =
-       -- IDs of stars of movies starring Kevin Bacon
-       (SELECT s1.person_id
-          FROM stars AS s1
-         WHERE s1.movie_id =
-               -- Movie IDs starring Kevin Bacon
-               (SELECT s2.movie_id
-                  FROM stars AS s2
-                       INNER JOIN people AS p2
-                       ON s2.person_id = p2.id
-                 WHERE p2.name = 'Kevin Bacon'
-                   AND p2.birth = 1958  -- Avoid other Kevin Bacons
-               )
+       INNER JOIN stars AS s1
+       ON p1.id = s1.person_id
+ WHERE s1.movie_id =
+       -- Movie IDs of movies starring Kevin Bacon
+       (SELECT s2.movie_id
+          FROM stars AS s2
+               INNER JOIN people AS p2
+               ON s2.person_id = p2.id
+         WHERE p2.name = 'Kevin Bacon'
+           AND p2.birth = 1958  -- Avoid other Kevin Bacons
        )
    AND p1.name <> 'Kevin Bacon';  -- Exclude Kevin Bacon from list
