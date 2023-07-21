0b4cd800fb7cb5bb16316e3bac37848ae8d9e8c9
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-21 19:46:28 +0000

    Fri, Jul 21, 2023, 3:46 PM -04:00

--- cs50/week9/birthdays/app.py
+++ cs50/week9/birthdays/app.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
 # Configure application
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
