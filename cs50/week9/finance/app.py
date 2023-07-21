994c8e03b078ce9cac4f9eb83497dc15e43ec298
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-21 20:22:32 +0000

    Fri, Jul 21, 2023, 4:22 PM -04:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -1,5 +1,14 @@
 import os
 
+import cs50
+import flask
+import flask_session
+import tempfile
+import werkzeug.security
+
+import helpers
+
+"""
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -7,20 +16,21 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
+"""
 
 # Configure application
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+app.jinja_env.filters["usd"] = helpers.usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+flask_session.Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = cs50.SQL("sqlite:///finance.db")
 
 
 @app.after_request
@@ -33,24 +43,24 @@ def after_request(response):
 
 
 @app.route("/")
-@login_required
+@helpers.login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    return helpers.apology("TODO")
 
 
 @app.route("/buy", methods=["GET", "POST"])
-@login_required
+@helpers.login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    return helpers.apology("TODO")
 
 
 @app.route("/history")
-@login_required
+@helpers.login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    return helpers.apology("TODO")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -58,35 +68,35 @@ def login():
     """Log user in"""
 
     # Forget any user_id
-    session.clear()
+    flask.session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+    if flask.request.method == "POST":
 
         # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        if not flask.request.form.get("username"):
+            return helpers.apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+        elif not flask.request.form.get("password"):
+            return helpers.apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = ?", flask.request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not werkzeug.security.check_password_hash(rows[0]["hash"], flask.request.form.get("password")):
+            return helpers.apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        flask.session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return flask.redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return flask.render_template("login.html")
 
 
 @app.route("/logout")
@@ -94,27 +104,27 @@ def logout():
     """Log user out"""
 
     # Forget any user_id
-    session.clear()
+    flask.session.clear()
 
     # Redirect user to login form
-    return redirect("/")
+    return flask.redirect("/")
 
 
 @app.route("/quote", methods=["GET", "POST"])
-@login_required
+@helpers.login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    return helpers.apology("TODO")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    return helpers.apology("TODO")
 
 
 @app.route("/sell", methods=["GET", "POST"])
-@login_required
+@helpers.login_required
 def sell():
     """Sell shares of stock"""
-    return apology("TODO")
+    return helpers.apology("TODO")
