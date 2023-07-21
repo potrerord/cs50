d3235a993215b37614924fdf1386de4a91aee42f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-21 13:26:26 +0000

    Fri, Jul 21, 2023, 9:26 AM -04:00

--- cs7/week5/pset/5a/5a.py
+++ cs7/week5/pset/5a/5a.py
@@ -1,11 +1,26 @@
 """
-# Q1
+# Q1.1
 y = set()
 y.add('a')
 y.add('a')
 print(y)
 """
 
+"""
+# Q1.2
 y = { 'b', 'a', 'b', 'c' }
 y.remove('b')
 print(y)
+"""
+
+"""
+# Q1.3
+y = { 'a', 'b' }
+y.remove('c')
+print(y)
+"""
+
+# Q1.4
+print('ab' in {'a', 'b', 'c'})
+
+
