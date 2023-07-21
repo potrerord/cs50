67fa5dae931ab77f02f39677ddb28fa419a07ab3
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-21 15:01:11 +0000

    Fri, Jul 21, 2023, 11:01 AM -04:00

--- cs7/week5/pset/5b/letters.py
+++ cs7/week5/pset/5b/letters.py
@@ -1,17 +1,34 @@
-"""Add your solution to the problem 'letters' here."""
-
-
-def missing_letters(word_list):
-    """Takes a word list and returns a sorted
-    list of letters, capitalized that do not appear
-    in the list.
-    """
-    # Add your code here.
+"""
+Prints an alphabetized list of letters not used by a particular list of
+words.
+"""
 
 
 def main():
-    # Add your solution to the problem that makes use of the above.
-    missing_letters(["Now", "is", "the", "TIME"])
+    """Test function."""
+
+    test_list = ["Now", "is", "the", "TIME"]
+    print(missing_letters(test_list))
+
+
+def missing_letters(word_list):
+    """Take a word list and return a sorted list of capitalized letters
+    that do not appear in the list.
+    """
+
+    # Initialize letters list.
+    letters = ['A', 'B', 'C', 'D', 'E', 'F',
+               'G', 'H', 'I', 'J', 'K', 'L',
+               'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X',
+               'Y', 'Z']
+
+    # Remove all matches.
+    for word in word_list:
+        for char in word:
+            letters.discard(char.upper())
+
+    return letters
 
 
 if __name__ == "__main__":
