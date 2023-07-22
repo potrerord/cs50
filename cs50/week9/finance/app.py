5436d37ace3f712990dddccad80d4fdd5a85aeed
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-22 23:39:15 +0000

    Sat, Jul 22, 2023, 7:39 PM -04:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -39,7 +39,7 @@ db = cs50.SQL("sqlite:///finance.db")
 
 
 @app.after_request
-def after_request(response):
+def after_request(response: flask.Response) -> flask.Response:
     """Ensure responses aren't cached."""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
@@ -52,7 +52,11 @@ def after_request(response):
 def index():
     """Show portfolio of stocks."""
 
+    """Complete the implementation of index in such a way that it displays an HTML table summarizing, for the user currently logged in, which stocks the user owns, the numbers of shares owned, the current price of each stock, and the total value of each holding (i.e., shares times price). Also display the user’s current cash balance along with a grand total (i.e., stocks’ total value plus cash).
 
+    Odds are you’ll want to execute multiple SELECTs. Depending on how you implement your table(s), you might find GROUP BY HAVING SUM and/or WHERE of interest.
+    Odds are you’ll want to call lookup for each stock.
+    """
 
 
 
@@ -68,7 +72,23 @@ def index():
 def buy():
     """Buy shares of stock."""
 
+    """Complete the implementation of buy in such a way that it enables a user to buy stocks.
 
+    Require that a user input a stock’s symbol, implemented as a text field whose name is symbol. Render an apology if the input is blank or the symbol does not exist (as per the return value of lookup).
+    Require that a user input a number of shares, implemented as a text field whose name is shares. Render an apology if the input is not a positive integer.
+    Submit the user’s input via POST to /buy.
+    Upon completion, redirect the user to the home page.
+    Odds are you’ll want to call lookup to look up a stock’s current price.
+    Odds are you’ll want to SELECT how much cash the user currently has in users.
+    Add one or more new tables to finance.db via which to keep track of the purchase. Store enough information so that you know who bought what at what price and when.
+    Use appropriate SQLite types.
+    Define UNIQUE indexes on any fields that should be unique.
+    Define (non-UNIQUE) indexes on any fields via which you will search (as via SELECT with WHERE).
+    Render an apology, without completing a purchase, if the user cannot afford the number of shares at the current price.
+    You don’t need to worry about race conditions (or use transactions).
+    Once you’ve implemented buy correctly, you should be able to see users’ purchases in your new table(s) via phpLiteAdmin or sqlite3.
+
+    """
 
 
 
@@ -87,7 +107,11 @@ def history():
     """Show history of transactions."""
 
 
+    """Complete the implementation of history in such a way that it displays an HTML table summarizing all of a user’s transactions ever, listing row by row each and every buy and every sell.
 
+    For each row, make clear whether a stock was bought or sold and include the stock’s symbol, the (purchase or sale) price, the number of shares bought or sold, and the date and time at which the transaction occurred.
+    You might need to alter the table you created for buy or supplement it with an additional table. Try to minimize redundancies.
+    """
 
 
 
@@ -156,7 +180,12 @@ def logout():
 def quote():
     """Get stock quote."""
 
+    """Complete the implementation of quote in such a way that it allows a user to look up a stock’s current price.
 
+    Require that a user input a stock’s symbol, implemented as a text field whose name is symbol.
+    Submit the user’s input via POST to /quote.
+    Odds are you’ll want to create two new templates (e.g., quote.html and quoted.html). When a user visits /quote via GET, render one of those templates, inside of which should be an HTML form that submits to /quote via POST. In response to a POST, quote can render that second template, embedding within it one or more values from lookup.
+    """
 
 
 
@@ -172,7 +201,15 @@ def quote():
 def register():
     """Register user."""
 
+    """Complete the implementation of register in such a way that it allows a user to register for an account via a form.
 
+    Require that a user input a username, implemented as a text field whose name is username. Render an apology if the user’s input is blank or the username already exists.
+    Require that a user input a password, implemented as a text field whose name is password, and then that same password again, implemented as a text field whose name is confirmation. Render an apology if either input is blank or the passwords do not match.
+    Submit the user’s input via POST to /register.
+    INSERT the new user into users, storing a hash of the user’s password, not the password itself. Hash the user’s password with generate_password_hash Odds are you’ll want to create a new template (e.g., register.html) that’s quite similar to login.html.
+    Once you’ve implemented register correctly, you should be able to register for an account and log in (since login and logout already work)! And you should be able to see your rows via phpLiteAdmin or sqlite3.
+
+    """
 
 
 
@@ -189,7 +226,14 @@ def register():
 def sell():
     """Sell shares of stock."""
 
+    """Complete the implementation of sell in such a way that it enables a user to sell shares of a stock (that he or she owns).
 
+    Require that a user input a stock’s symbol, implemented as a select menu whose name is symbol. Render an apology if the user fails to select a stock or if (somehow, once submitted) the user does not own any shares of that stock.
+    Require that a user input a number of shares, implemented as a text field whose name is shares. Render an apology if the input is not a positive integer or if the user does not own that many shares of the stock.
+    Submit the user’s input via POST to /sell.
+    Upon completion, redirect the user to the home page.
+    You don’t need to worry about race conditions (or use transactions).
+    """
 
 
 
