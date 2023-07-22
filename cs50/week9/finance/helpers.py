28837a864552b8a46e1ce4d19b39b30cb09b052f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-22 22:23:52 +0000

    Sat, Jul 22, 2023, 6:23 PM -04:00

--- cs50/week9/finance/helpers.py
+++ cs50/week9/finance/helpers.py
@@ -8,6 +8,7 @@ import uuid
 
 import flask
 import functools
+
 """
 from flask import redirect, render_template, session
 from functools import wraps
@@ -32,11 +33,11 @@ def apology(message, code=400):
             ("%", "~p"),
             ("#", "~h"),
             ("/", "~s"),
-            ('"', "''")
+            ('"', "''"),
         ]:
             s = s.replace(old, new)
         return s
-    
+
     return flask.render_template("apology.html", top=code, bottom=escape(message)), code
 
 
@@ -46,11 +47,13 @@ def login_required(f):
 
     http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
     """
+
     @functools.wraps(f)
     def decorated_function(*args, **kwargs):
         if flask.session.get("user_id") is None:
             return flask.redirect("/login")
         return f(*args, **kwargs)
+
     return decorated_function
 
 
@@ -72,18 +75,18 @@ def lookup(symbol):
 
     # Query API
     try:
-        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        response = requests.get(
+            url,
+            cookies={"session": str(uuid.uuid4())},
+            headers={"User-Agent": "python-requests", "Accept": "*/*"},
+        )
         response.raise_for_status()
 
         # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
         quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
         quotes.reverse()
         price = round(float(quotes[0]["Adj Close"]), 2)
-        return {
-            "name": symbol,
-            "price": price,
-            "symbol": symbol
-        }
+        return {"name": symbol, "price": price, "symbol": symbol}
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
