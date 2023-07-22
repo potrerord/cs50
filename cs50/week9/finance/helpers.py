f7a2b18f248974e4e26762adf425dca4d12e057e
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-22 23:20:47 +0000

    Sat, Jul 22, 2023, 7:20 PM -04:00

--- cs50/week9/finance/helpers.py
+++ cs50/week9/finance/helpers.py
@@ -19,7 +19,7 @@ from functools import wraps
 def apology(message: str, code: int = 400):
     """Render message as an apology to the user."""
 
-    def escape(s):
+    def escape(s: str):
         """Escape special characters.
 
         https://github.com/jacebrowning/memegen#special-characters
