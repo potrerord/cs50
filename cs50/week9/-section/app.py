6622791219e49e7d4ae1f44a1eda4c1fd4f33b47
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-22 15:36:56 +0000

    Sat, Jul 22, 2023, 11:36 AM -04:00

--- cs50/week9/-section/app.py
+++ cs50/week9/-section/app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 # Initiate instance of Flask class, conventionally named "app".
 app = Flask(__name__)
 
-
+# 
 @app.route("/")
 def current():
     time = datetime.now()
