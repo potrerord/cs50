eec48d6e4a4b4e1cbd6725e33f3346808650c566
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-22 15:30:08 +0000

    Sat, Jul 22, 2023, 11:30 AM -04:00

--- cs50/week9/-section/1.py
+++ cs50/week9/-section/1.py
@@ -3,7 +3,7 @@
 """
 
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 # Initiate instance of Flask class, conventionally named "app".
 app = Flask(__name__)
@@ -11,5 +11,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def now():
-    now = datetime.now()
-    return render_template()
+    time = datetime.now()
+    return render_template("index.html", time=time)
