d95cd3f3553f7495310895902289f13cee07ac45
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-23 15:29:33 +0000

    Sun, Jul 23, 2023, 11:29 AM -04:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -232,7 +232,7 @@ def register() -> flask.Response:
             return helpers.apology("Passwords do not match.")
 
         # Apologize if password does not match requirements.
-        if re.search(r"[A-Z]", )
+        if re.search(r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)", form_password)
 
         # Hash password before saving it into database.
         hashed_form_password = werkzeug.security.generate_password_hash(form_password)
