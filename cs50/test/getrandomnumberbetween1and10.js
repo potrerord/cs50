066abfd8f33c71e1a2b831ef00257f05766636da
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 17:31:31 +0000

    Tue, Jul 25, 2023, 1:31 PM -04:00

--- cs50/test/getrandomnumberbetween1and10.js
+++ cs50/test/getrandomnumberbetween1and10.js
@@ -9,16 +9,3 @@ function getrandomnumberbetween1and10() {
 
     return number;
 }
-
-let n = 1000000000;
-let dictionary = {};
-for (let i = 0; i < n; i++) {
-    num = getrandomnumberbetween1and10();
-    if (!dictionary.hasOwnProperty(num)) {
-        dictionary[num] = 0;
-    }
-
-    dictionary[num] += 1;
-}
-
-console.log(dictionary)
