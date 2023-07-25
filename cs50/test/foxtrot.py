9faa78ba5865082ab48029a8f1ce4cb1cec09ea6
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 18:07:06 +0000

    Tue, Jul 25, 2023, 2:07 PM -04:00

--- cs50/test/foxtrot.py
+++ cs50/test/foxtrot.py
@@ -1,12 +1,16 @@
 """
-Writes lines on the blackboard.
+Writes blackboard "lines" akin to the FoxTrot comic in the CS50 Test.
 """
 
 
 def main():
+    """Emulate the exact behavior of the FoxTrot comic."""
+
+    # Iterate using a variable named "count" from 1 to 500.
     for count in range(1, 501):
         print("I will not throw paper airplanes in class.")
 
+    # Return an integer value of 0 as per the FoxTrot comic.
     return 0
 
 
