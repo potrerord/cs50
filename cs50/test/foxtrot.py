e41ccf0e6797931ccfe76df0556af9d67a2ceeb2
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 18:02:44 +0000

    Tue, Jul 25, 2023, 2:02 PM -04:00

---
+++ cs50/test/foxtrot.py
@@ -0,0 +1,12 @@
+"""
+Writes lines on the blackboard.
+"""
+
+
+def main():
+    for count in range(1, 501):
+        print("I will not throw paper airplanes in class.")
+
+
+if __name__ == "__main__":
+    main()
