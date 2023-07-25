c590b02c2039d6b4dc5ef88a9053eed03dbf9aa0
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 15:13:21 +0000

    Tue, Jul 25, 2023, 11:13 AM -04:00

--- cs7/week5/pset/5e/selection.py
+++ cs7/week5/pset/5e/selection.py
@@ -10,10 +10,22 @@ def main():
 
 
 
-def selection_sort(data: List[int]):
+def selection_sort(data: List[int], start=0) -> None:
     """Sort a list using the selection sort algorithm."""
 
-    
+    # Base case: if there are no remaining data points to sort, return.
+    if start == len(data) - 1:
+        return
+
+    # Scan each data point to find the smallest value.
+    lowest = data[start]
+    for element in data[start:]:
+        if element < lowest:
+            lowest = i
+            lowest_index =
+
+    # Sort again starting from the next position.
+    selection_sort(data, start + 1)
 
 
 if __name__ == "__main__":
