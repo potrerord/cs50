1963bb2499ed2bdc36452f4500868ac440ad94da
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 17:09:17 +0000

    Tue, Jul 25, 2023, 1:09 PM -04:00

--- cs50/test/getrandomnumberbetween1and10.js
+++ cs50/test/getrandomnumberbetween1and10.js
@@ -4,12 +4,8 @@ function getrandomnumberbetween1and10() {
     // Generate random float where 0 <= rand_float < 1.
     let rand_float = Math.random();
 
-    // Scale rand_float to 0 <= rand_float < 10 and save int floor.
-    let number = Math.floor(rand_float * 10)
+    // Scale rand_float to 0 <= rand_float < 10, floor it, then add 1.
+    let number = Math.floor(rand_float * 10) + 1
 
-    // 
-    if (number == 0) {
-        number = 10
-    }
     return number
 }
