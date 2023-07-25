a0d04e8f24c2f4d3ee0ce7891d289254cc066a1d
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 17:07:14 +0000

    Tue, Jul 25, 2023, 1:07 PM -04:00

--- cs50/test/getrandomnumberbetween1and10.js
+++ cs50/test/getrandomnumberbetween1and10.js
@@ -1,6 +1,15 @@
 // JavaScript style guide: https://github.com/airbnb/javascript
 
 function getrandomnumberbetween1and10() {
-    let number = Math.random();
-    
+    // Generate random float where 0 <= rand_float < 1.
+    let rand_float = Math.random();
+
+    // Scale rand_float to 0 <= rand_float < 10 and save int floor.
+    let number = Math.floor(rand_float * 10)
+
+    // 
+    if (number == 0) {
+        number = 10
+    }
+    return number
 }
