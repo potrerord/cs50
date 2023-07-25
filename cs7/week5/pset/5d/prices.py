91a9d2c6551653fad05d938ee45511e0551d87ac
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-25 14:04:39 +0000

    Tue, Jul 25, 2023, 10:04 AM -04:00

--- cs7/week5/pset/5d/prices.py
+++ cs7/week5/pset/5d/prices.py
@@ -13,11 +13,11 @@ def main():
 
 
     # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-r = requests.get(url)
-data = r.json()
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+    r = requests.get(url)
+    data = r.json()
 
-print(data)
+    print(data)
 
 
 
