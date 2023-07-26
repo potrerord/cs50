61a10a4c073de52dcd3fd0b17e8bfe8f1f9b393f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 21:03:07 +0000

    Wed, Jul 26, 2023, 5:03 PM -04:00

--- cs50/test/js/readability.js
+++ cs50/test/js/readability.js
@@ -2,13 +2,15 @@
 
 // Prints the Coleman-Liau index reading grade level for input text.
 
+const test = "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since...."
+
 // Import the readline-sync module to interact with user via console.
 const readlineSync = require('readline-sync');
 
 // Main function.
 const main = function mainFunction () {
   // Prompt user for text to analyze
-  let userString = readlineSync.question("Enter text: ");
+  let userString = test // readlineSync.question("Enter text: ");
 
   // Initiate counter variables.
   let userLetters = 0;
