4db9eb3bb940c78311c1b9566ad9989bf59d081c
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 22:33:28 +0000

    Wed, Jul 26, 2023, 6:33 PM -04:00

--- cs50/test/jack/push.c
+++ cs50/test/jack/push.c
@@ -1,13 +1,15 @@
 bool push(int n)
 {
+    // If size is already maxed out, return false.
     if (s.size >= CAPACITY)
     {
         return false;
     }
 
+    // Start at last available
     for (int i = s.size - 1; i > -1; i--)
     {
-        s.numbers[i + 1] = s.numbers[i];
+        s.numbers[i] = s.numbers[i - 1];
     }
 
     s.numbers[0] = n;
