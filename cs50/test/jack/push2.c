4be6e3971fd2bf338ff158bfbc20fae1e0c6dfe1
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 23:19:35 +0000

    Wed, Jul 26, 2023, 7:19 PM -04:00

--- cs50/test/jack/push2.c
+++ cs50/test/jack/push2.c
@@ -3,11 +3,12 @@ bool push(int n)
     // If size is already maxed out, return false.
     if (s.size >= CAPACITY)
     {
-        return false;
+        s.size += 1;
+        ptr = realloc(ptr, s.size)
     }
 
     // Change the value at the next available index to n.
-    s.numbers[s.size] = n;
+    s->numbers[s.size] = n;
 
     // Increment the size of the stack.
     s.size += 1;
