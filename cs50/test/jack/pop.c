04e0c982fbdfb96b736a760834265cfb943fb67b
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 21:50:28 +0000

    Wed, Jul 26, 2023, 5:50 PM -04:00

--- cs50/test/jack/pop.c
+++ cs50/test/jack/pop.c
@@ -6,10 +6,10 @@ bool pop(int *n)
 
     n = s[0];
 
-    for (int i = s.size; )
-
+    for (int i = 0, len = s.size; i < len - 1; i++) {
+        s[i] = s[i + 1]
+    }
 
     s.size--;
     return true;
-
 }
