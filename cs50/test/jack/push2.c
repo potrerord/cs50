b802335c2af53433cb9e95580638bf3d2944f27f
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 23:16:40 +0000

    Wed, Jul 26, 2023, 7:16 PM -04:00

--- cs50/test/jack/push2.c
+++ cs50/test/jack/push2.c
@@ -1,4 +1,16 @@
 bool push(int n)
 {
-    // TODO
+    // If size is already maxed out, return false.
+    if (s.size >= CAPACITY)
+    {
+        return false;
+    }
+
+    // Change the value at the next available index to n.
+    s.numbers[s.size] = n;
+
+    // Increment the size of the stack.
+    s.size += 1;
+
+    return true;
 }
