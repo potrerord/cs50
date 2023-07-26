a546e1a14bec77d71ab2a9c4c6875863108756cf
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 21:30:58 +0000

    Wed, Jul 26, 2023, 5:30 PM -04:00

--- cs50/test/jack/push.c
+++ cs50/test/jack/push.c
@@ -1,9 +1,15 @@
 bool push(int n)
 {
-    int temp;
+    if (s.size == s.CAPACITY) {
+        return false;
+    }
+
+    int temp, temp2;
+    
     for (int i = 0, size = s.size; i < size; i++) {
         temp = s[i];
+        temp2 = s[i + 1]
         s[i] = n;
-        s[i + 1] = 
+        s[i + 1] = s[i]
     }
 }
