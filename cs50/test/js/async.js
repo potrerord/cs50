779684cf01424fd2a17e99ceec36f191cb3277d4
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-26 21:12:08 +0000

    Wed, Jul 26, 2023, 5:12 PM -04:00

--- cs50/test/js/async.js
+++ cs50/test/js/async.js
@@ -1,6 +1,19 @@
 // Airbnb JavaScript style guide: https://github.com/airbnb/javascript/
 
+// Says hello.
 
+// Main function.
+const main = function mainFunction () {
+  // Request string input from user.
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
+  readline.question(`What is your name? `, name => {
+    console.log(`hello, ${name}`);
+    readline.close();
+  });
+}
 
-
+main()
