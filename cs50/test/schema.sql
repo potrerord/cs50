5d6837e215b4a4ddf4b534bd836bf82ec1fdb316
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-27 12:27:32 +0000

    Thu, Jul 27, 2023, 8:27 AM -04:00

--- cs50/test/schema.sql
+++ cs50/test/schema.sql
@@ -1,6 +1,6 @@
 -- SQL Style Guide: https://www.sqlstyle.guide/
 
 CREATE TABLE warehouses (
-    PRIMARY KEY (id),
-    id      
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT
 );
