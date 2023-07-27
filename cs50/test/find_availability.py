99c83a54ab1a5820b1a90407f95e7ff44d8ba057
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-27 12:57:13 +0000

    Thu, Jul 27, 2023, 8:57 AM -04:00

--- cs50/test/find_availability.py
+++ cs50/test/find_availability.py
@@ -1,8 +1,16 @@
+"""
+Locates Amazon warehouses that have a specific item in stock.
+"""
+
+from typing import Dict, List, Union
+
 from cs50 import SQL
 
 db = SQL("sqlite:///amazon.db")
 
+def find_availability(item_id: int) -> List[Dict[int, Union[int, float]]]:
+    """Return a list of warehouses in which an item with `item_id` is in
+    stock."""
 
-def find_availability(item_id):
-    """Returns a list of warehouses in which an item with `item_id` is in stock"""
     
+
