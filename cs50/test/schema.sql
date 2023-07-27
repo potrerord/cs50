39660df0fa380ec8786a6431d37d253823c632af
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-27 12:34:12 +0000

    Thu, Jul 27, 2023, 8:34 AM -04:00

--- cs50/test/schema.sql
+++ cs50/test/schema.sql
@@ -1,8 +1,9 @@
 -- SQL Style Guide: https://www.sqlstyle.guide/
 
 CREATE TABLE warehouses (
-    id      INTEGER NOT NULL,
-    name    TEXT NOT NULL,
-    latitude 
+    id        INTEGER NOT NULL,
+    name      TEXT    NOT NULL,
+    latitude  REAL    NOT NULL,
+    longitude REAL    NOT NULL,
     PRIMARY KEY(id)
 );
