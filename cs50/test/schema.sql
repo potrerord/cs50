b2ced241210ed3f68c2a54a8e6de2e5fb30c0992
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-27 12:37:22 +0000

    Thu, Jul 27, 2023, 8:37 AM -04:00

--- cs50/test/schema.sql
+++ cs50/test/schema.sql
@@ -1,9 +1,19 @@
 -- SQL Style Guide: https://www.sqlstyle.guide/
 
 CREATE TABLE warehouses (
-    id        INTEGER NOT NULL,
+    id        INTEGER NOT NULL AUTOINCREMENT,
     name      TEXT    NOT NULL,
     latitude  REAL    NOT NULL,
     longitude REAL    NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE items (
+    id           INTEGER NOT NULL AUTOINCREMENT,
+    name         TEXT    NOT NULL,
+    quantity     INTEGER NOT NULL,
+    warehouse_id INTEGER NOT NULL,
+    FOREIGN KEY (warehouse_id) REFERENCES warehouses(id),
+    PRIMARY KEY (id)
+);
+
