ebc6e642297f839b3298a9dde8fdfee4c38d01fc
potrerord <125526503+potrerord@users.noreply.github.com>
2023-07-27 12:59:24 +0000

    Thu, Jul 27, 2023, 8:59 AM -04:00

--- cs50/test/find_availability.py
+++ cs50/test/find_availability.py
@@ -12,5 +12,11 @@ def find_availability(item_id: int) -> List[Dict[int, Union[int, float]]]:
     """Return a list of warehouses in which an item with `item_id` is in
     stock."""
 
-    
+    available_warehouses = db.execute(
+        """
+        SELECT *
+          FROM warehouses AS w
+               JOIN 
+        """
+    )
 
